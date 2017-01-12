3dd513fbd03c7ba86661fd2b5a98424fe26fed32
Patrick Walsh <patrick@revature.com>
2017-01-12 14:12:19 -0500

    configure pipeline

--- assessments/src/test/java/com/revature/caliber/initial/ApplicationContextTest.java
+++ assessments/src/test/java/com/revature/caliber/initial/ApplicationContextTest.java
@@ -1,14 +1,27 @@
 package com.revature.caliber.initial;
 
-import static org.junit.Assert.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class ApplicationContextTest {
 
 	@Test
-	public void context(){
-		assertNotNull(new FileSystemXmlApplicationContext("src/main/webapp/WEB-INF/beans.xml"));
+	public void context() {
+		try {
+			ApplicationContext ctxt = new FileSystemXmlApplicationContext("src/main/webapp/WEB-INF/beans.xml");
+			DataSource ds = (DataSource) ctxt.getBean("dataSource");
+			Connection conn = ds.getConnection();
+			System.out.println("Connection " + conn);
+			Assert.assertNotNull(conn);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
-	
 }
