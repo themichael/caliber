367e1ffcf898a244d81312dbec11ce8620fed1a5
Karina Macancela <karymacancela@gmail.com>
2017-01-18 12:59:38 -0500

    hibernate annotations

--- training/src/main/java/com/revature/caliber/training/beans/Tier.java
+++ training/src/main/java/com/revature/caliber/training/beans/Tier.java
@@ -1,12 +1,16 @@
 package com.revature.caliber.training.beans;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
+@Entity
+@Table(name="CALIBER_TIER")
 public class Tier {
 
 	@Id
