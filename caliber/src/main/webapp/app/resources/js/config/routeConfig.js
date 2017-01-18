ea1cb541eceea62bac817253bd18f9dd17948a66
Rikki Katz <katz.rikki@gmail.com>
2017-01-18 16:19:00 -0500

    update routes

--- caliber/src/main/webapp/app/resources/js/config/routeConfig.js
+++ caliber/src/main/webapp/app/resources/js/config/routeConfig.js
@@ -1,10 +1,19 @@
 angular.module("app").config(function ($stateProvider, $locationProvider, $urlRouterProvider) {
     // go to home on startup
-    $urlRouterProvider.otherwise('test');
+    $urlRouterProvider.otherwise('/routing');
     // app states
-    $stateProvider.state("qc", {
+    $stateProvider.state("routing", {
+    	url: "/routing",
+   		templateUrl: "app/partials/helloWorld.html"
+    })
+    .state("qc", {
         abstract: true,
         url: "/qc",
         templateUrl: "app/partials/qc.html"
+    })
+    .state("trainer", {
+    	abstract: true,
+    	url: "/trainer",
+    	templateUrl: "app/partials/abstracts/trainer.html"
     });
 });
