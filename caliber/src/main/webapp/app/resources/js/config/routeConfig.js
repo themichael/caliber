2d0485b254d88e40130a5bb4a03fbc1beccd1076
Kyle Chang Fatt <kchangfatt@gmail.com>
2017-01-18 17:48:13 -0500

    Fixed routing

--- caliber/src/main/webapp/app/resources/js/config/routeConfig.js
+++ caliber/src/main/webapp/app/resources/js/config/routeConfig.js
@@ -1,72 +1,41 @@
-angular.module("app").config(function ($stateProvider, $locationProvider, $urlRouterProvider) {
-    // go to home on startup
-    $urlRouterProvider.otherwise('/routing');
+angular.module("app").config(
+		function($stateProvider, $locationProvider, $urlRouterProvider) {
+			// go to home on startup
+			$urlRouterProvider.otherwise('/routing');
 
-    // app states
-    $stateProvider
-        .state("routing", {
-            url: "/routing",
-            templateUrl: "app/partials/helloWorld.html"
-        })
+			// app states
+			$stateProvider.state("routing", {
+				url : "/routing",
+				templateUrl : "app/partials/helloWorld.html"
+			})
 
-        // qc
-        .state("qc", {
-            abstract: true,
-            url: "/qc",
-            templateUrl: "app/partials/abstracts/qc.html"
-        })
-        .state("qc.home", {
-            url: "/home",
-            templateUrl: "app/partials/abstracts/qc-home.html"
-        })
-        // trainer
-        .state("trainer", {
-            abstract: true,
-            url: "/trainer",
-            templateUrl: "app/partials/abstracts/trainer.html"
-        })
-        // vp
-        .state("vp", {
-            abstract: true,
-            templateUrl: "app/partials/abstracts/vp.html",
-            url: "/vp"
-        })
-        .state("vp.home", {
-            templateUrl: "app/partials/home/vp-home.html",
-            url: "/home"
-        });
-    $stateProvider.state("routing", {
-    	url: "/routing",
-   		templateUrl: "app/partials/helloWorld.html"
-    })
+			// qc
+			.state("qc", {
+				abstract : true,
+				url : "/qc",
+				templateUrl : "app/partials/abstracts/qc.html"
+			}).state("qc.home", {
+				url : "/home",
+				templateUrl : "app/partials/abstracts/qc-home.html"
+			})
+			// trainer
+			.state("trainer", {
+				abstract : true,
+				url : "/trainer",
+				templateUrl : "app/partials/abstracts/trainer.html"
+			}).state("trainer.home", {
+				templateUrl : "app/partials/home/trainer-home.html",
+				url : "/home"
+			})
+			
+			// vp
+			.state("vp", {
+				abstract : true,
+				templateUrl : "app/partials/abstracts/vp.html",
+				url : "/vp"
+			}).state("vp.home", {
+				templateUrl : "app/partials/home/vp-home.html",
+				url : "/home"
+			});
 
-    // qc
-    .state("qc", {
-        abstract: true,
-        url: "/qc",
-        templateUrl: "app/partials/abstracts/qc.html"
-    })
-
-    // trainer
-    .state("trainer", {
-    	abstract: true,
-    	url: "/trainer",
-    	templateUrl: "app/partials/abstracts/trainer.html"
-    })
-    .state("trainer.home", {
-    	templateUrl: "app/partials/home/trainer-home.html",
-		url: "/home"
-    })
-
-    // vp
-    .state("vp", {
-		abstract: true,
-		templateUrl: "app/partials/abstracts/vp.html",
-		url: "/vp"
-	})
-	.state("vp.home", {
-		templateUrl: "app/partials/home/vp-home.html",
-		url: "/home",
-		controller: "vpHomeController"
-	});
-});
+		});
