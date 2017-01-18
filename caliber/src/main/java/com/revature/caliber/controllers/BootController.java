ccd6d9a1b8a0940647781cd4ddb22041c2598fb8
Rikki Katz <katz.rikki@gmail.com>
2017-01-18 10:04:05 -0500

    added index and boot controller

---
+++ caliber/src/main/java/com/revature/caliber/controllers/BootController.java
@@ -0,0 +1,14 @@
+package com.revature.caliber.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BootController {
+	
+	@RequestMapping(value={"/index"}, method=RequestMethod.GET)
+	public String homePage(){
+		return "index"; 
+	}
+}
