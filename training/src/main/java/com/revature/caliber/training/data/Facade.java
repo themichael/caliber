0d4ef4b127d9dfcf8485dcec0ff76c03c5591a54
oiale <ilysiarh@gmail.com>
2017-01-18 15:18:07 -0500

    Added methods to Facade

--- training/src/main/java/com/revature/caliber/training/data/Facade.java
+++ training/src/main/java/com/revature/caliber/training/data/Facade.java
@@ -1,7 +1,19 @@
 package com.revature.caliber.training.data;
 
+import com.revature.caliber.training.beans.Trainee;
+
+import java.util.List;
+
 /**
  * Facade interface for data tier.
  */
 public interface Facade {
+
+    public void createTrainee(Trainee trainee);
+    public void updateTrainee(Trainee trainee);
+    public Trainee getTrainee(Integer id);
+    public Trainee getTrainee(String name);
+    public List<Trainee> getTraineesInBatch(Integer batchId);
+    public void deleteTrainee(Trainee trainee);
+
 }
