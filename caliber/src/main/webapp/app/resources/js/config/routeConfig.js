be0514d22b0179563fd7d812a4eb1a28df73939f
Kyle Chang Fatt <kchangfatt@gmail.com>
2017-01-18 16:23:25 -0500

    Setup route for vp pages

--- caliber/src/main/webapp/app/resources/js/config/routeConfig.js
+++ caliber/src/main/webapp/app/resources/js/config/routeConfig.js
@@ -1,13 +1,21 @@
 angular.module("app").config(function($stateProvider, $locationProvider, $urlRouterProvider){
 	// go to home on startup
-	$urlRouterProvider.otherwise('test');
+	$urlRouterProvider.otherwise('vp/home');
 	
 	// app states
 	$stateProvider
-		// public area
 		.state("test",{
 			url: "/test",
 			templateUrl: "app/partials/main.html",
 		})
+		.state("vp", {
+			abstract: true,
+			templateUrl: "app/partials/vp.html",
+			url: "/vp"
+		})
+		.state("vp.home", {
+			templateUrl: "app/partials/vp-home.html",
+			url: "/home"
+		});
   
 });
