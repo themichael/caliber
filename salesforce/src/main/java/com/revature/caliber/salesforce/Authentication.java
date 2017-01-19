cc155928f7418e11f42678447635a627d2065a2f
louislopez <louisalopez93@gmail.com>
2017-01-19 09:06:19 -0500

    Got access token & refresh token

--- salesforce/src/main/java/com/revature/caliber/salesforce/Authentication.java
+++ salesforce/src/main/java/com/revature/caliber/salesforce/Authentication.java
@@ -1,19 +1,25 @@
 package com.revature.caliber.salesforce;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by louislopez on 1/18/17.
@@ -33,36 +39,64 @@ public class Authentication {
     private String clientId;
     @Value("${clientSecret}")
     private String clientSecret;
-    @Value("${redirectUri}")
-    private String redirectUri;
+    @Value("${redirectCodeUri}")
+    private String redirectCodeUri;
+    @Value("${redirectTokentUri}")
+    private String redirectTokentUri;
+
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertyConfig() {
         return new PropertySourcesPlaceholderConfigurer();
     }
 
 
-    public String generateURL(){
-        return authURL+"response_type=code&client_id="
-                +clientId+"&redirect_uri="+redirectUri;
+    public String generateURL() {
+        return authURL + "response_type=code&client_id="
+                + clientId + "&redirect_uri=" + redirectCodeUri;
     }
 
 
     @RequestMapping(value = "/auth")
     public ModelAndView openAuth() {
-        System.out.println("IN AUTH");
-        return new ModelAndView("redirect:"+generateURL());
+        return new ModelAndView("redirect:" + generateURL());
     }
 
-    @RequestMapping(value = "/token")
-    public String getToken() {
-        System.out.println("Got token");
-        return "TO";
+    @RequestMapping(value = "/code")
+    public void getCode(@RequestParam(value = "code") String code) {
+        try {
+            HttpClient httpClient = HttpClientBuilder.create().build();
+            HttpPost post = new HttpPost(accessTokenURL);
+            List<NameValuePair> parameters = new ArrayList<NameValuePair>();
+            parameters.add(new BasicNameValuePair("grant_type", "authorization_code"));
+            parameters.add(new BasicNameValuePair("client_secret", clientSecret));
+            parameters.add(new BasicNameValuePair("client_id", clientId));
+            parameters.add(new BasicNameValuePair("redirect_uri", redirectCodeUri));
+            parameters.add(new BasicNameValuePair("code", code));
+            post.setEntity(new UrlEncodedFormEntity(parameters));
+            HttpResponse response = httpClient.execute(post);
 
+            System.out.println("RESPONSE CODE " + response.getStatusLine().getStatusCode());
+
+            BufferedReader resp = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            StringBuffer result = new StringBuffer();
+            String line = resp.readLine();
+
+            while (line != null) {
+                result.append(line);
+                line = resp.readLine();
+            }
+
+            System.out.println(result.toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 
     @ResponseBody
     @RequestMapping(value = "test")
-    public String test(){
+    public String test() {
         return "TEST :)";
     }
 
