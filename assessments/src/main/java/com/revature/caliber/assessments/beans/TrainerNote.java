06569c7951fe2c345490c7b18191ab91deebab89
ambitious1 <travis.dacosta1@gmail.com>
2017-01-19 14:51:27 -0500

    added methods to the facade and service methods

--- assessments/src/main/java/com/revature/caliber/assessments/beans/TrainerNote.java
+++ assessments/src/main/java/com/revature/caliber/assessments/beans/TrainerNote.java
@@ -2,21 +2,23 @@ package com.revature.caliber.assessments.beans;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 @Entity(name="TRAINER_NOTE")
 public class TrainerNote extends Note{
+	//Get the ID for the trainer to set the owner of the Note 
 	@Id
 	@Column(name="TRAINER_ID")
-	@NotNull
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@JoinColumn(name="TRAINER_ID")
+	@OneToOne
 	private int trainer;
 	
-	@Column(name="WEEK_ID")
-	@NotNull
+	//Get the ID for the week to set which week the note is regarding 
+	@Column(name="WEEK_ID", nullable=false)
+	@JoinColumn(name="WEEK_ID")
+	@OneToOne
 	private int week;
 	
 	
--- assessments/src/main/java/com/revature/caliber/assessments/data/TrainerNoteDAO.java
+++ assessments/src/main/java/com/revature/caliber/assessments/data/TrainerNoteDAO.java
@@ -1,12 +1,18 @@
 package com.revature.caliber.assessments.data;
 
-import java.util.HashSet;
+import java.util.List;
 
 import com.revature.caliber.assessments.beans.TrainerNote;
 
 public interface TrainerNoteDAO {
+	
+	//Create a TrainerNote based on the ID of the trainer
 	void createTrainerNote(int trainerId);
-	HashSet<TrainerNote> getAllTrainerNotesByTrainer(int trainerId);
+	
+	//Get a List of all the TrainerNotes ever written by a specific Trainer 
+	List<TrainerNote> getAllTrainerNotesByTrainer(int trainerId);
+	
+	//Get a TrainerNote from a Trainer for a specific week
 	TrainerNote getTrainerNoteForWeek(int trainerId, int weekId);
 	
 }
