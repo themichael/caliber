3840c8a7537fd97c032e610b37ca0a8a83a4da83
Martino Nikolovski <nikolovski23@gmail.com>
2017-01-18 23:59:31 -0500

    Changed the salesforce api to use environment variables

--- salesforce/src/main/java/com/revature/caliber/salesforce/Authentication.java
+++ salesforce/src/main/java/com/revature/caliber/salesforce/Authentication.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * Created by louislopez on 1/18/17.
@@ -21,49 +22,35 @@ import java.io.IOException;
 
 
 @Controller
-@PropertySource("classpath:dev_salesforce.properties")
 public class Authentication {
+    Map<String, String> environment = System.getenv();
+    private String authURL = environment.get("SALESFORCE_AUTH_URL");
+    private String accessTokenURL= environment.get("SALESFORCE_ACCESS_TOKEN_URL");
+    private String clientId = environment.get("SALESFORCE_CLIENT_ID");
+    private String clientSecret = environment.get("SALESFORCE_CLIENT_SECRET");
+    private String redirectUri = environment.get("SALESFORCE_REDIRECT_URI");
 
-
-    @Value("${authURL}")
-    private String authURL;
-    @Value("${accessTokenURL}")
-    private String accessTokenURL;
-    @Value("${clientId}")
-    private String clientId;
-    @Value("${clientSecret}")
-    private String clientSecret;
-    @Value("${redirectUri}")
-    private String redirectUri;
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertyConfig() {
         return new PropertySourcesPlaceholderConfigurer();
     }
 
-
-    public String generateURL(){
-        return authURL+"response_type=code&client_id="
-                +clientId+"&redirect_uri="+redirectUri;
+    public String generateURL() {
+        return authURL + "?response_type=code&client_id="
+                + clientId + "&redirect_uri=" + redirectUri;
     }
 
 
     @RequestMapping(value = "/auth")
     public ModelAndView openAuth() {
         System.out.println("IN AUTH");
-        return new ModelAndView("redirect:"+generateURL());
+        return new ModelAndView("redirect:" + generateURL());
     }
 
-    @RequestMapping(value = "/token")
+    @RequestMapping(value = "/code")
     public String getToken() {
         System.out.println("Got token");
         return "TO";
 
     }
-
-    @ResponseBody
-    @RequestMapping(value = "test")
-    public String test(){
-        return "TEST :)";
-    }
-
 }
