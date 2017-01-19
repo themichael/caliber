a505bdf03e2559a5c6b9f82ca067618997e3dcda
Ychen192 <ychenq001@gmail.com>
2017-01-19 17:02:27 -0500

    commit before leaving

--- assessments/src/main/java/com/revature/caliber/assessments/beans/QCStatus.java
+++ assessments/src/main/java/com/revature/caliber/assessments/beans/QCStatus.java
@@ -3,49 +3,51 @@ package com.revature.caliber.assessments.beans;
 import javax.persistence.*;
 import java.util.Set;
 
-
 @Entity
-@Table(name="CALIBER_QC_STATUS")
+@Table(name = "CALIBER_QC_STATUS")
 public class QCStatus {
-	
+
 	@Id
-    @Column(name = "QC_STATUS_ID")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "QC_STATUS_ID_SEQUENCE")
-    @SequenceGenerator(name = "QC_STATUS_ID_SEQUENCE", sequenceName = "QC_STATUS_ID_SEQUENCE")
+	@Column(name = "QC_STATUS_ID")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "QC_STATUS_ID_SEQUENCE")
+	@SequenceGenerator(name = "QC_STATUS_ID_SEQUENCE", sequenceName = "QC_STATUS_ID_SEQUENCE")
 	private short statusId;
-	
+
 	@Column(name = "QC_STATUS_STATUS")
 	private String status;
-	
+
 	@OneToMany(mappedBy = "weeklyStatus")
 	private Set<Assessment> assessments;
-
 
 	public short getStatusId() {
 		return statusId;
 	}
+
 	public void setStatusId(short statusId) {
 		this.statusId = statusId;
 	}
+
 	public String getStatus() {
 		return status;
 	}
+
 	public void setStatus(String status) {
 		this.status = status;
 	}
+
 	public QCStatus(String status) {
 		super();
 		this.status = status;
 	}
+
 	public QCStatus(short statusId, String status) {
 		super();
 		this.statusId = statusId;
 		this.status = status;
 	}
+
 	public QCStatus() {
 		super();
 	}
-	
-	
-	
+
 }
--- assessments/src/main/java/com/revature/caliber/assessments/data/implementations/QCStatusDAOImpl.java
+++ assessments/src/main/java/com/revature/caliber/assessments/data/implementations/QCStatusDAOImpl.java
@@ -1,8 +1,25 @@
 package com.revature.caliber.assessments.data.implementations;
 
+import org.hibernate.SessionFactory;
+
 import com.revature.caliber.assessments.data.QCStatusDAO;
 
 public class QCStatusDAOImpl implements QCStatusDAO {
 
+	SessionFactory sessionFactory;
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	@Override
+	public void getAll() {
+
+	}
+
+	@Override
+	public void getByID(short statusId) {
+
+	}
 
 }
