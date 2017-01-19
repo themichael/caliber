f02c35bed4c3fd025b78b361c542be93229af8cd
Andrew Chen <achen.this@gmail.com>
2017-01-19 09:10:02 -0500

    Added additional services and implementations in middle tier in Assessment module

--- assessments/src/main/java/com/revature/caliber/assessments/service/implementations/BusinessDelegateImpl.java
+++ assessments/src/main/java/com/revature/caliber/assessments/service/implementations/BusinessDelegateImpl.java
@@ -1,52 +1,99 @@
 package com.revature.caliber.assessments.service.implementations;
 
 import com.revature.caliber.assessments.beans.Assessment;
-import com.revature.caliber.assessments.data.Facade;
-import com.revature.caliber.assessments.service.BusinessDelegate;
+import com.revature.caliber.assessments.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.HashSet;
 
-public class BusinessDelegateImpl implements BusinessDelegate{
+public class BusinessDelegateImpl implements BusinessDelegate {
 
-    private Facade facade;
+    private AssessmentService assessmentService;
+    private BatchService batchService;
+    private CategoryService categoryService;
+    private GradeService gradeService;
+    private NoteService noteService;
+    private QCNoteService qcNoteService;
+    private QCStatusService qcStatusService;
+    private TrainerNoteService trainerNoteService;
 
-//    Spring setter based DI
-    public void setFacade(Facade facade) {
-        this.facade = facade;
-    }
-
+//    Assessment
     @Override
     public HashSet<Assessment> getAllAssessments() {
-        return null;
+        return assessmentService.getAllAssessments();
     }
 
     @Override
     public HashSet<Assessment> getAssessmentsByTrainerId(int id) {
-        return null;
+        return assessmentService.getAssessmentsByTrainerId(id);
     }
 
     @Override
     public HashSet<Assessment> getAssessmentsByWeekId(int id) {
-        return null;
+        return assessmentService.getAssessmentsByWeekId(id);
     }
 
     @Override
     public HashSet<Assessment> getAssessmentsByBatchId(int id) {
-        return null;
+        return assessmentService.getAssessmentsByBatchId(id);
     }
 
     @Override
     public void insertAssessment(Assessment assessment) {
-
+        assessmentService.insertAssessment(assessment);
     }
 
     @Override
     public void updateAssessment(Assessment assessment) {
-
+        assessmentService.updateAssessment(assessment);
     }
 
     @Override
     public void deleteAssessment(Assessment assessment) {
-
+        assessmentService.deleteAssessment(assessment);
     }
+
+//    Batch
+
+
+
+
+
+
+
+
+//    Spring setter based DI
+    @Autowired
+    public void setAssessmentService(AssessmentService assessmentService) {
+        this.assessmentService = assessmentService;
+    }
+    @Autowired
+    public void setBatchService(BatchService batchService) {
+        this.batchService = batchService;
+    }
+    @Autowired
+    public void setCategoryService(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+    @Autowired
+    public void setGradeService(GradeService gradeService) {
+        this.gradeService = gradeService;
+    }
+    @Autowired
+    public void setNoteService(NoteService noteService) {
+        this.noteService = noteService;
+    }
+    @Autowired
+    public void setQcNoteService(QCNoteService qcNoteService) {
+        this.qcNoteService = qcNoteService;
+    }
+    @Autowired
+    public void setQcStatusService(QCStatusService qcStatusService) {
+        this.qcStatusService = qcStatusService;
+    }
+    @Autowired
+    public void setTrainerNoteService(TrainerNoteService trainerNoteService) {
+        this.trainerNoteService = trainerNoteService;
+    }
+
 }
