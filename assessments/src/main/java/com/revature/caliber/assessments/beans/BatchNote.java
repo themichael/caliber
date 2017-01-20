fb26ffb3231ddb996b4db5cb4cc3f10105a859c3
Patrick Walsh <patrick@revature.com>
2017-01-20 10:59:13 -0500

    wip

--- assessments/src/main/java/com/revature/caliber/assessments/beans/BatchNote.java
+++ assessments/src/main/java/com/revature/caliber/assessments/beans/BatchNote.java
@@ -12,13 +12,11 @@ public class BatchNote extends Note{
 	//Use the WeekID from the Week Table and put it for the week of the BatchNote 
 	@Id
 	@Column(name="WEEK_ID", nullable=false)
-	@JoinColumn(name="WEEK_ID")
 	@OneToOne
 	private int week;
 	
 	//Use the BatchID from the the Batch Table and use it for the specific Batch
 	@Column(name="BATCH_ID")
-	@JoinColumn(name="BATCH_ID")
 	private int batch;
 	
 	public int getWeek() {
