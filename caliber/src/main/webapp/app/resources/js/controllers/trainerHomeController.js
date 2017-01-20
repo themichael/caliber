866846eb25a49aa530d71b80cc312cdc465597b8
Rikki Katz <katz.rikki@gmail.com>
2017-01-20 10:51:15 -0500

    add pie chart and legends

--- caliber/src/main/webapp/app/resources/js/controllers/trainerHomeController.js
+++ caliber/src/main/webapp/app/resources/js/controllers/trainerHomeController.js
@@ -1,55 +1,86 @@
 angular.module("trainer").controller(
 		"trainerHomeController",
-		function($scope, $log, $interval) {
+		function($scope, $log) {
 			$log.info("Booted trainer home controller.");
 
-			$scope.batches = [ "Batch1311", "Batch1612", "Batch1512", "Batch1812", "Batch0910", "Batch0805", "Batch0408" ];
+			$scope.batches = [ "Batch1311", "Batch1612", "Batch1512",
+					"Batch1812", "Batch0910", "Batch0805", "Batch0408" ];
 			$scope.tech = [ "Spring", "Hibernate", "JSP" ];
 			$scope.trainees = [ "Osher", "Kyle", "Rikki" ];
-			
+
 			$scope.currentBatch = $scope.batches[0];
-			
+
 			$scope.currentTech = "Technology";
-				
+
 			$scope.currentTrainee = "Trainee";
-			
-			$scope.selectCurrentBatch = function(index){
+
+			$scope.selectCurrentBatch = function(index) {
 				$scope.currentBatch = $scope.batches[index];
 			};
-			
-			$scope.selectCurrentTech = function(index){
+
+			$scope.selectCurrentTech = function(index) {
 				$scope.currentTech = $scope.tech[index];
 			};
-			
-			$scope.selectCurrentTrainee = function(index){
+
+			$scope.selectCurrentTrainee = function(index) {
 				$scope.currentTrainee = $scope.trainees[index];
 			};
-			
-			// Show week by week progression for a trainee on a line chart
-			$scope.lineLabels = ["Week 1", "Week 2", "Week 3", "Week 4", "Week 5", 
-			                       "Week 6", "Week 7", "Week 8", "Week 9", "Week 10", 
-			                       "Week 11", "Week 12"];
-			$scope.lineSeries = [ 'Average' ];
-			$scope.lineData = [ [79, 89, 67, 79, 86, 76, 90, 51 ] ];
-			$scope.lineOnClick = function(points, evt) {
-				console.log(points, evt);
-			};
+
+			// Week by week progression for a trainee/ batch on a line chart
+			$scope.lineLabels = [ "Week 1", "Week 2", "Week 3", "Week 4",
+					"Week 5", "Week 6", "Week 7", "Week 8", "Week 9",
+					"Week 10", "Week 11", "Week 12" ];
+			$scope.lineSeries = [ 'Average', 'Fail' ];
+			$scope.lineData = [ [ 79, 89, 67, 79, 86, 76, 90, 51 ],
+					[ 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40 ] ];
 			$scope.lineDatasetOverride = [ {
 				yAxisID : 'y-axis-1',
-				fill: false
-			}];
+				fill : false
+			}, {
+				yAxisID : 'y-axis-2',
+				backgroundColor : 'rgba(255, 0, 0, .5)',
+				borderColor : 'rgba(255, 0, 0, .5)',
+				pointBorderColor : false,
+				pointBackgroundColor : false,
+				pointRadius : 0,
+				pointHoverRadius : 0
+			} ];
 			$scope.lineOptions = {
+				legend : {
+					display : true,
+					position: 'bottom'
+				},
 				scales : {
-					yAxes : [{
+					yAxes : [ {
 						id : 'y-axis-1',
 						type : 'linear',
 						display : true,
 						position : 'left',
-						ticks: {
-		                    min: 0,
-		                    max: 100
-		                }
-					}]
+						ticks : {
+							min : 30,
+							max : 100
+						}
+					}, {
+						id : 'y-axis-2',
+						type : 'linear',
+						display : false,
+						position : 'left',
+						ticks : {
+							min : 30,
+							max : 100
+						}
+					} ]
 				}
 			};
+
+			// Pie chart for trainee's technology strength
+			$scope.pieLabels = [ "Spring", "Hibernate", "AngularJS",
+					"Java Core", "SQL" ];
+			$scope.pieData = [ 50, 80, 90, 85, 75 ];
+			$scope.pieOptions = {
+				legend : {
+					display : true,
+					position: 'left'
+				}
+			}
 		});
