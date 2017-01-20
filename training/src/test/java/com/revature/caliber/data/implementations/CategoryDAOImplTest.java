0d4ad45671ab086b0ca3279834004619fe88bf9c
Andrew Williams <williams.andrew15@gmail.com>
2017-01-20 10:47:38 -0500

    Test for CategoryDAOImpl

--- training/src/test/java/com/revature/caliber/data/implementations/CategoryDAOImplTest.java
+++ training/src/test/java/com/revature/caliber/data/implementations/CategoryDAOImplTest.java
@@ -3,6 +3,8 @@ package com.revature.caliber.data.implementations;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -15,34 +17,40 @@ import com.revature.caliber.training.data.Facade;
 public class CategoryDAOImplTest {
 
 	private static ApplicationContext contxt;
+	private Logger logger;
 
     @BeforeClass
     public static void preClass () {
         contxt = new FileSystemXmlApplicationContext("src/main/webapp/WEB-INF/beans.xml");
     }
-    
+	
+	@Before
+	public void configureLogger(){
+		this.logger = Logger.getRootLogger();
+	}
+	
     //Testing the CategoryDAOImplementation
     @Test
     public void getCategory(){
     	Category category = contxt.getBean(CategoryDAO.class).getCategory(1);
-    	System.out.println(category + "First Test");
+    	logger.info(category + "First Test");
     }
     
     @Test
     public void getAllCategories(){
     	List<Category> categories = new ArrayList<Category>();
     	categories = contxt.getBean(CategoryDAO.class).getAllCategories();
-    	System.out.println(categories + "Second Test");
+    	logger.info(categories + "Second Test");
     }
     
     //Testing the category methods in the Facade
     @Test
     public void getCategoryFacade(){
-    	System.out.println(contxt.getBean(Facade.class).getCategory(1) + "Third Test");
+    	logger.info(contxt.getBean(Facade.class).getCategory(1) + "Third Test");
     }
     
     @Test
     public void getAllCategoryFacade(){
-    	System.out.println(contxt.getBean(Facade.class).getAllCategories() + "Fourth Test");
+    	logger.info(contxt.getBean(Facade.class).getAllCategories() + "Fourth Test");
     }
 }
