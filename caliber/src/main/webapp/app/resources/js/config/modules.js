d5d8f18ac823861e339aa115d6b3c64ca817dba3
Rikki Katz <katz.rikki@gmail.com>
2017-01-20 15:02:16 -0500

    add factories and bar chart

--- caliber/src/main/webapp/app/resources/js/config/modules.js
+++ caliber/src/main/webapp/app/resources/js/config/modules.js
@@ -5,3 +5,4 @@ angular.module("app", ["ui.router", "vp", "qc", "trainer", "chart.js"]);
 angular.module("vp", []);
 angular.module("qc", []);
 angular.module("trainer", []);
+angular.module("charts", []);
