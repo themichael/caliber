3ff7c8c0dcf7d57dd313761180ba262e8508fc5d
Kyle Chang Fatt <kchangfatt@gmail.com>
2017-01-20 05:41:20 -0500

    Edited landing page template. Added dynamic list to drop down menu.

--- caliber/src/main/webapp/app/resources/js/controllers/qcHomeController.js
+++ caliber/src/main/webapp/app/resources/js/controllers/qcHomeController.js
@@ -1,5 +1,23 @@
 angular.module("qc").controller("qcHomeController", function($scope) {
-	$scope.batches = [ "first batch", "second batch", "third batch" ];
-	$scope.technologies = [ "Spring", "Hibernate", "JSP" ];
+	$scope.batches = [ "Batch1311", "Batch1612", "Batch1512", "Batch1812", "Batch0910", "Batch0805", "Batch0408" ];
+	$scope.tech = [ "Spring", "Hibernate", "JSP" ];
 	$scope.trainees = [ "Osher", "Kyle", "Rikki" ];
+	
+	$scope.currentBatch = $scope.batches[0];
+	
+	$scope.currentTech = "Technology";
+		
+	$scope.currentTrainee = "Trainee";
+	
+	$scope.selectCurrentBatch = function(index){
+		$scope.currentBatch = $scope.batches[index];
+	};
+	
+	$scope.selectCurrentTech = function(index){
+		$scope.currentTech = $scope.tech[index];
+	};
+	
+	$scope.selectCurrentTrainee = function(index){
+		$scope.currentTrainee = $scope.trainees[index];
+	};
 });
--- caliber/src/main/webapp/app/resources/js/controllers/trainerHomeController.js
+++ caliber/src/main/webapp/app/resources/js/controllers/trainerHomeController.js
@@ -3,6 +3,28 @@ angular.module("trainer").controller(
 		function($scope, $log, $interval) {
 			$log.info("Booted trainer home controller.");
 
+			$scope.batches = [ "Batch1311", "Batch1612", "Batch1512", "Batch1812", "Batch0910", "Batch0805", "Batch0408" ];
+			$scope.tech = [ "Spring", "Hibernate", "JSP" ];
+			$scope.trainees = [ "Osher", "Kyle", "Rikki" ];
+			
+			$scope.currentBatch = $scope.batches[0];
+			
+			$scope.currentTech = "Technology";
+				
+			$scope.currentTrainee = "Trainee";
+			
+			$scope.selectCurrentBatch = function(index){
+				$scope.currentBatch = $scope.batches[index];
+			};
+			
+			$scope.selectCurrentTech = function(index){
+				$scope.currentTech = $scope.tech[index];
+			};
+			
+			$scope.selectCurrentTrainee = function(index){
+				$scope.currentTrainee = $scope.trainees[index];
+			};
+			
 			// Show week by week progression for a trainee on a line chart
 			$scope.lineLabels = ["Week 1", "Week 2", "Week 3", "Week 4", "Week 5", 
 			                       "Week 6", "Week 7", "Week 8", "Week 9", "Week 10", 
