2d6ef918e6e0650b49154cf4b7fb6dd32d991945
oiale <ilysiarh@gmail.com>
2017-01-20 13:30:44 -0500

    Creating transient tests

--- training/src/test/java/com/revature/caliber/data/implementations/TraineeDAOImplementationTest.java
+++ training/src/test/java/com/revature/caliber/data/implementations/TraineeDAOImplementationTest.java
@@ -4,6 +4,9 @@ import com.revature.caliber.training.beans.Batch;
 import com.revature.caliber.training.beans.Trainee;
 import com.revature.caliber.training.data.TraineeDAO;
 import org.apache.log4j.Logger;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -21,13 +24,69 @@ import static org.junit.Assert.*;
 public class TraineeDAOImplementationTest {
 
     private static ApplicationContext context;
+    private static SessionFactory sf;
     private static Logger logger;
+
+    private static int newBatchId, newTrainerId, newCategoryId;
 
     @BeforeClass
     public static void preClass () {
         context = new FileSystemXmlApplicationContext("src/main/webapp/WEB-INF/beans.xml");
-        logger = Logger.getRootLogger();
 
+
+        /*
+        CALIBER_TIER - > TIER_ID, RANKING, TIER;
+        CALIBER_TRAINER - > TRAINER_ID, EMAIL, NAME, SF_ACCOUNT, SF_AUTHENTICATION_TOKEN, SF_REFRESH_TOKEN, TITLE, TIER(int)
+        CALIBER_BATCH -> BATCH_ID, BORDERLINE_GRADE_THRESHOLD, END_DATE, GOOD_GRADE_THRESHOLD, LOCATION, SKILL_TYPE, START_DATE, TRAINING_NAME, TRAINING_TYPE, CO_TRAINER_ID, TRAINER_ID
+         */
+        //create transient SQL test data
+        sf = (SessionFactory) context.getBean("sessionFactory");
+        Session session = sf.openSession();
+        String sql = "";
+        int index = 0;
+        int resultNum = 0;
+
+        index = 1;
+        do {
+            sql = "SELECT count(*) from CALIBER_TIER where TIER_ID  = ?";
+            Query q = session.createSQLQuery(sql);
+            q.setInteger(1, index++);
+            resultNum = q.getFirstResult();
+            break;
+        } while (resultNum > 0);
+
+        newCategoryId = index - 1;
+
+        sql = "INSERT INTO CALIBER_TIER(TIER_ID, RANKING, TIER) VALUES(?, ?, ?)";
+        Query tierq = session.createSQLQuery(sql);
+        tierq.setInteger(1, newCategoryId);
+        tierq.setInteger(2, 1);
+        tierq.setString(3, "Test tier (TraineeDAO Test)");
+
+        resultNum = tierq.executeUpdate();
+
+        if (resultNum != 1) {
+            fail("Failed to create test tier");
+        }
+
+        index = 1;
+        do {
+            sql = "SELECT count(*) FROM CALIBER_TRAINER WHERE TRAINER_ID = ?";
+            Query q = session.createSQLQuery(sql);
+        } while (resultNum > 0);
+
+        newTrainerId = index - 1;
+        sql = "INSERT INTO CALIBER_CATEGORY() VALUES();" +
+                    "INSERT INTO CALIBER_TRAINER() VALUES();" +
+                    "INSERT INTO CALIBER_BATCH() VALUES();";
+        Query query = session.createSQLQuery(sql);
+        int rowsAffected = query.executeUpdate();
+        if (rowsAffected < 1) {
+            fail("Failed to create test data.");
+        }
+        session.close();
+
+        logger = Logger.getRootLogger();
         logger.debug("\n--- TRAINEE DAO IMPLEMENTATION TEST START ---\n");
     }
 
@@ -41,7 +100,7 @@ public class TraineeDAOImplementationTest {
     public void createTraineeTest() {
         logger.debug("   Create trainee test.");
 
-        TraineeDAO dao = (TraineeDAO) context.getBean("trainingTraineeDAOImplementation");
+        TraineeDAO dao = (TraineeDAO) context.getBean(TraineeDAO.class);
 
         Batch batch = new Batch();
         batch.setBatchId(1);
