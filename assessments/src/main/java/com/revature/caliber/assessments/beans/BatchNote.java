2ae6b7104fc6c4dcb65af9878494687beffd8a35
Patrick Walsh <patrick@revature.com>
2017-01-20 11:09:07 -0500

    wip

--- assessments/src/main/java/com/revature/caliber/assessments/beans/BatchNote.java
+++ assessments/src/main/java/com/revature/caliber/assessments/beans/BatchNote.java
@@ -8,13 +8,16 @@ import javax.persistence.OneToOne;
 
 @Entity(name="CALIBER_BATCH_NOTE")
 public class BatchNote extends Note{
-	
-	//Use the WeekID from the Week Table and put it for the week of the BatchNote 
-	@Id
+	 
+	/**
+	 * The week of the batch's evaluation
+	 */
 	@Column(name="WEEK_ID", nullable=false)
 	private int week;
 	
-	//Use the BatchID from the the Batch Table and use it for the specific Batch
+	/**
+	 * The batch identifier
+	 */
 	@Column(name="BATCH_ID")
 	private int batch;
 	
