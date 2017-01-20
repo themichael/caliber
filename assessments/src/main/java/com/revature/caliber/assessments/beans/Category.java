d2522e5f65f985cc2609edf9fce9d574d73a1be7
Patrick Walsh <patrick@revature.com>
2017-01-20 11:49:02 -0500

    wip

--- assessments/src/main/java/com/revature/caliber/assessments/beans/Category.java
+++ assessments/src/main/java/com/revature/caliber/assessments/beans/Category.java
@@ -1,6 +1,9 @@
 package com.revature.caliber.assessments.beans;
 
+import java.util.Set;
+
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -8,8 +11,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.SequenceGenerator;
-
-import java.util.Set;
 
 @Entity(name = "CALIBER_ASSESSMENT_CATEGORY")
 public class Category {
@@ -27,6 +28,8 @@ public class Category {
     @ManyToMany(fetch = FetchType.LAZY, mappedBy = "categories")
     private Set<Assessment> assessments;
 
+    @Column
+    @ElementCollection(targetClass=Integer.class)
     private Set<Integer> weeks;
 
     public Category() {
