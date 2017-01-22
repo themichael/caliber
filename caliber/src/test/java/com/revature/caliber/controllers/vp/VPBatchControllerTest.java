1af04f4eb2d3dee15554763b9f989c56916ef698
Kyle Chang Fatt <kchangfatt@gmail.com>
2017-01-22 16:33:11 -0500

    edited java doc for each unit test

--- caliber/src/test/java/com/revature/caliber/controllers/vp/VPBatchControllerTest.java
+++ caliber/src/test/java/com/revature/caliber/controllers/vp/VPBatchControllerTest.java
@@ -19,12 +19,19 @@ public class VPBatchControllerTest {
 	private static ApplicationContext context;
 	private static VPBatchController controller;
 	
+	/**
+	 * Retrieve application context and get the VPBatchController bean
+	 */
 	@Before
 	public void preClass(){
 		context = new FileSystemXmlApplicationContext("src/main/webapp/WEB-INF/beans.xml");
 		controller = context.getBean(VPBatchController.class);
 	}
 	
+	/**
+	 * 	Tests vp batch controller method(getAllBatches)
+	 * 	- test that all batches are returned
+	 */
 	@Test
 	public void getAllBatches(){
 		ResponseEntity<Set<Batch>> enitity = controller.getAllBatches();
@@ -34,6 +41,11 @@ public class VPBatchControllerTest {
 		assertEquals(3, set.size());
 	}
 	
+	/**
+	 *  Tests vp batch controller method(getAllCurrentBatches)
+	 *  
+	 *  - test that all current batches are returned
+	 */
 	@Test
 	public void getAllCurrentBatches(){
 		ResponseEntity<Set<Batch>> enitity = controller.getAllCurrentBatches();
@@ -43,6 +55,11 @@ public class VPBatchControllerTest {
 		assertEquals(3, set.size());
 	}
 	
+	/*
+	 * 	Tests vp batch controller method(getBatch)
+	 * 
+	 * 	- tests that a batch with the given id is returned
+	 */
 	@Test
 	public void getBatch(){
 		int batchId = 5;
@@ -53,6 +70,11 @@ public class VPBatchControllerTest {
 		assertEquals(batchId, batch.getBatchId());
 	}
 	
+	/**
+	 * 	Tests vp batch controller method(getCurrentBatch)
+	 * 
+	 * 	- tests that a current batch with the given id is returned
+	 */
 	@Test
 	public void getCurrentBatch(){
 		int batchId = 6;
