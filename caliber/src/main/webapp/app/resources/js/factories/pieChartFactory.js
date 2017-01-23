8bd3101b9cef25addf64eddcaefb7c2d3ffe3871
Kyle Chang Fatt <kchangfatt@gmail.com>
2017-01-23 11:26:01 -0500

    removed unused code

--- caliber/src/main/webapp/app/resources/js/factories/pieChartFactory.js
+++ caliber/src/main/webapp/app/resources/js/factories/pieChartFactory.js
@@ -1,50 +1,6 @@
 angular.module("charts").factory("pieChartFactory", function($log) {
-	var lineChart = {};
+	var pieChart = {};
 
-	/**
-	 * Getters and Setters for pie chart objects
-	 */
-	// get/ set labels
-	pieChart.setLabels = funtion(labels)
-	{
-		charts.labels = labels;
-	}
-	;
-	pieChart.getLabels = funtion()
-	{
-		if (!charts.labels)
-			return null;
-		return charts.labels;
-	}
-	;
-
-	// get/ set series
-	pieChart.setSeries = funtion(series)
-	{
-		charts.series = series;
-	}
-	;
-	pieChart.getSeries = funtion()
-	{
-		if (!charts.series)
-			return null;
-		return charts.series;
-	}
-	;
-
-	// get/ set data
-	pieChart.setData = funtion(data)
-	{
-		charts.data = data;
-	}
-	;
-	pieChart.getData = funtion()
-	{
-		if (!charts.data)
-			return null;
-		return charts.data;
-	}
-	;
-
+	
 	return pieChart;
 });
