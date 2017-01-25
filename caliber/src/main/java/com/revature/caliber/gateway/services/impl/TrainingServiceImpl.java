a93a15d35db1bf21d058ba26fdc9e98bbcec5342
Bryan Cantos <bcantos17@gmail.com>
2017-01-25 10:46:07 -0500

    Implemented create, getall, getbytrainer and getcurrent methods

--- caliber/src/main/java/com/revature/caliber/gateway/services/impl/TrainingServiceImpl.java
+++ caliber/src/main/java/com/revature/caliber/gateway/services/impl/TrainingServiceImpl.java
@@ -44,50 +44,60 @@ public class TrainingServiceImpl implements TrainingService{
 
 	@Override
 	public List<Batch> allBatch() {
-/*		RestTemplate service = new RestTemplate();
+		RestTemplate service = new RestTemplate();
 		// Build Service URL
 		final String URI = UriComponentsBuilder.fromHttpUrl(hostname + portNumber).path(allBatch)
 						.build().toUriString();
 		// Invoke the service
-		ResponseEntity<Batch[]> response =
-				service.getForEntity(URI, Batch[].class);
+		ResponseEntity<Batch[]> response = service.getForEntity(URI, Batch[].class);
+
 		if(response.getStatusCode() == HttpStatus.BAD_REQUEST){
-			// TODO Create custom runtime exception
-			throw new RuntimeException("Trainer not found.");
+			throw new RuntimeException("Batches not found.");
 		}else if(response.getStatusCode() == HttpStatus.OK){
 			return Arrays.asList(response.getBody());
 		}else {
 			// Includes 404 and other responses. Give back no data.
-			return new ArrayList<Batch>();
-		}*/
-return null;
+			return new ArrayList<>();
+		}
 	}
 
 	@Override
 	public List<Batch> getBatches(Trainer trainer) {
 		RestTemplate service = new RestTemplate();
 		// Build Service URL
-		final String URI = 
-				UriComponentsBuilder.fromHttpUrl(hostname + portNumber
-						+ allBatchesForTrainer).path(trainer.getName())
-				.build().toUriString();
+		final String URI = UriComponentsBuilder.fromHttpUrl(hostname + portNumber + allBatchesForTrainer)
+				.path(trainer.getName()).build().toUriString();
+
 		// Invoke the service
-		ResponseEntity<Batch[]> response =
-				service.getForEntity(URI, Batch[].class);
+		ResponseEntity<Batch[]> response = service.getForEntity(URI, Batch[].class);
+
 		if(response.getStatusCode() == HttpStatus.BAD_REQUEST){
-			// TODO Create custom runtime exception
 			throw new RuntimeException("Trainer not found.");
 		}else if(response.getStatusCode() == HttpStatus.OK){
 			return Arrays.asList(response.getBody());
 		}else {
 			// Includes 404 and other responses. Give back no data.
-			return new ArrayList<Batch>();
+			return new ArrayList<>();
 		}
 	}
 
 	@Override
 	public List<Batch> currentBatch() {
-		return null;
+		RestTemplate service = new RestTemplate();
+		// Build Service URL
+		final String URI = UriComponentsBuilder.fromHttpUrl(hostname + portNumber).path(allCurrentBatch)
+				.build().toUriString();
+		// Invoke the service
+		ResponseEntity<Batch[]> response = service.getForEntity(URI, Batch[].class);
+
+		if(response.getStatusCode() == HttpStatus.BAD_REQUEST){
+			throw new RuntimeException("No Current batches.");
+		}else if(response.getStatusCode() == HttpStatus.OK){
+			return Arrays.asList(response.getBody());
+		}else {
+			// Includes 404 and other responses. Give back no data.
+			return new ArrayList<>();
+		}
 	}
 
 	@Override
