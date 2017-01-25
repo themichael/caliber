9eb2f5ef07c0ab4cc61ff2a6dc0daac07499ed5f
Martino Nikolovski <nikolovski23@gmail.com>
2017-01-25 18:27:55 -0500

    Removed unnecessary classes for salesforce

--- salesforce/src/main/java/com/revature/caliber/salesforce/controllers/Salesforce.java
+++ salesforce/src/main/java/com/revature/caliber/salesforce/controllers/Salesforce.java
@@ -1,5 +1,6 @@
 package com.revature.caliber.salesforce.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
@@ -9,6 +10,24 @@ import java.io.IOException;
  */
 public interface Salesforce {
 
+    /**
+     * Generates authURI with provided parameters
+     * @return view of the generated URI
+     */
     ModelAndView openAuthURI();
-    void authenticate(String code) throws IOException;
+
+    /**
+     * Creates salesforce token in memory
+     * @param code the string returned from the authURI required for getting token from salesforce
+     * @throws IOException
+     */
+    void generateSalesforceToken(String code) throws IOException;
+
+    /**
+     * Gets the in memory salesforce user
+     * @return JSON representation of the salesforceUser
+     * @throws NullPointerException if the user is not authenticated with salesforce
+     * @throws JsonProcessingException if the salesforce API mapping is changed
+     */
+    String getSalesforceUser() throws NullPointerException, IOException;
 }
--- salesforce/src/main/java/com/revature/caliber/salesforce/controllers/SalesforceAPI.java
+++ salesforce/src/main/java/com/revature/caliber/salesforce/controllers/SalesforceAPI.java
@@ -2,30 +2,24 @@ package com.revature.caliber.salesforce.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.caliber.salesforce.models.SalesforceToken;
-import com.revature.caliber.salesforce.models.SalesforceUser;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
-import org.springframework.stereotype.Controller;
+import org.springframework.context.annotation.Scope;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by louislopez on 1/18/17.
@@ -33,6 +27,7 @@ import java.util.Map;
 
 
 @RestController
+@Scope(value = "session")
 public class SalesforceAPI implements Salesforce {
     private String authURL;
     private String accessTokenURL;
@@ -40,11 +35,41 @@ public class SalesforceAPI implements Salesforce {
     private String clientSecret;
     private String redirectUri;
     private SalesforceToken salesforceToken;
-    private SalesforceUser salesforceUser;
     private HttpClient httpClient;
+    private HttpResponse response;
 
     public SalesforceAPI() {
         httpClient = HttpClientBuilder.create().build();
+    }
+
+    @RequestMapping("/")
+    public ModelAndView openAuthURI() {
+        return new ModelAndView("redirect:" + authURL +
+                "?response_type=code&client_id=" + clientId +
+                "&redirect_uri=" + redirectUri);
+    }
+
+    @RequestMapping("/authenticated")
+    public void generateSalesforceToken(@RequestParam(value = "code") String code) throws IOException {
+            HttpPost post = new HttpPost(accessTokenURL);
+            List<NameValuePair> parameters = new ArrayList<>();
+            parameters.add(new BasicNameValuePair("grant_type", "authorization_code"));
+            parameters.add(new BasicNameValuePair("client_secret", clientSecret));
+            parameters.add(new BasicNameValuePair("client_id", clientId));
+            parameters.add(new BasicNameValuePair("redirect_uri", redirectUri));
+            parameters.add(new BasicNameValuePair("code", code));
+            post.setEntity(new UrlEncodedFormEntity(parameters));
+            response = httpClient.execute(post);
+            salesforceToken =
+                    new ObjectMapper().readValue(response.getEntity().getContent(), SalesforceToken.class);
+    }
+
+    @RequestMapping(value = "/getSalesforceUser", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getSalesforceUser() throws NullPointerException, IOException {
+        HttpGet get = new HttpGet(salesforceToken.getId() + "?access_token=" +
+                salesforceToken.getAccessToken());
+        response = httpClient.execute(get);
+        return response.getEntity().getContent().toString() ;
     }
 
     public void setAuthURL(String authURL) {
@@ -66,33 +91,4 @@ public class SalesforceAPI implements Salesforce {
     public void setRedirectUri(String redirectUri) {
         this.redirectUri = redirectUri;
     }
-
-    @RequestMapping(value = "/")
-    public ModelAndView openAuthURI() {
-        return new ModelAndView("redirect:" + authURL + "?response_type=code&client_id="
-                + clientId + "&redirect_uri=" + redirectUri);
-    }
-
-    @RequestMapping(value = "/authenticated")
-    public void authenticate(@RequestParam(value = "code") String code) throws IOException {
-            HttpPost post = new HttpPost(accessTokenURL);
-            List<NameValuePair> parameters = new ArrayList<>();
-            parameters.add(new BasicNameValuePair("grant_type", "authorization_code"));
-            parameters.add(new BasicNameValuePair("client_secret", clientSecret));
-            parameters.add(new BasicNameValuePair("client_id", clientId));
-            parameters.add(new BasicNameValuePair("redirect_uri", redirectUri));
-            parameters.add(new BasicNameValuePair("code", code));
-            post.setEntity(new UrlEncodedFormEntity(parameters));
-            HttpResponse response = httpClient.execute(post);
-            salesforceToken = new ObjectMapper().readValue(response.getEntity().getContent(), SalesforceToken.class);
-            setSalesforceUser(salesforceToken.getId());
-    }
-
-    public void setSalesforceUser(String str) throws IOException {
-        HttpGet get = new HttpGet(str + "?access_token=" + salesforceToken.getAccess_token());
-        HttpResponse response = httpClient.execute(get);
-        salesforceUser = new ObjectMapper().readValue(response.getEntity().getContent(),SalesforceUser.class);
-        salesforceUser.setSalesforceToken(salesforceToken);
-    }
-
 }
