cc01d2529aa2558e4609519a8cc38b793633d652
Rikki Katz <katz.rikki@gmail.com>
2017-01-27 11:54:46 -0500

    configure angular for create batch

--- caliber/src/main/webapp/app/resources/js/controllers/trainerManageController.js
+++ caliber/src/main/webapp/app/resources/js/controllers/trainerManageController.js
@@ -3,11 +3,52 @@ angular.module("trainer").controller(
     function ($scope, $log) {
         $log.debug("Booted trainer manage controller.");
 
-        $scope.trainingTypeOptions = ['Option A', 'Option B', 'Option C'];
-
-        $scope.trainingType = $scope.trainingTypeOptions[0];
-        $scope.selectTrainingType = function (index) {
-            $scope.trainingType = $scope.trainingTypeOptions[index];
+        $scope.batches=[{batchId: 1, trainingName: 'Batch123', trainingType: 'CUNY', skillType: 'Java', location: 'Queens, NY',
+                trainer: 'Patrick', coTrainer: '', startDate: new Date(), endDate: new Date()},
+            {batchId: 2, trainingName: 'Batch456', trainingType: 'Corporate', skillType: 'Java', location: 'Reston, VA',
+                trainer: 'Ryan', coTrainer: 'Brian', startDate: new Date(), endDate: new Date()}
+        ];
+        $scope.trainingName={
+            model: null
         };
-
+        $scope.trainingType = {
+            model: null,
+            options: ['Corporate', 'CUNY', 'Other']
+        };
+        $scope.skillType = {
+            model: null,
+            options: ['Java', '.Net', 'C#']
+        };
+        $scope.location = {
+            model: null,
+            options: ['Reston, VA', 'Queens, NY', 'Manhattan, NY']
+        };
+        $scope.trainer = {
+            model: null,
+            options: ['Patrick', 'Joe', 'Brian', 'Ryan']
+        };
+        $scope.coTrainer = {
+            model: null,
+            options: ['Karan', 'Steven', 'Nick']
+        };
+        $scope.receivers=[{value:""}];
+        $scope.addRecipient = function(receiver) {
+            $scope.receivers.push({value:""});
+        };
+        $scope.startDate={
+            model: null
+        };
+        $scope.endDate={
+            model: null
+        };
+        $scope.addNewBatch = function( ){
+            $scope.batches.push({trainingName: $scope.trainingName.model,
+                                trainingType: $scope.trainingType.model,
+                                skillType: $scope.skillType.model,
+                                location: $scope.location.model,
+                                trainer: $scope.trainer.model,
+                                coTrainer: $scope.coTrainer.model,
+                                startDate: $scope.startDate.model,
+                                endDate: $scope.endDate.model})
+            };
     });
