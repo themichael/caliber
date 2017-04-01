7ff3287f5e450998aa80b4b183a0db45afea9119
Patrick Walsh <patrick.walsh@revature.com>
2017-04-01 00:25:28 -0400

    configure security

--- caliber/src/main/java/com/revature/caliber/controllers/TrainingController.java
+++ caliber/src/main/java/com/revature/caliber/controllers/TrainingController.java
@@ -1,18 +1,21 @@
 package com.revature.caliber.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.caliber.beans.Batch;
 import com.revature.caliber.beans.Trainer;
 import com.revature.caliber.security.models.SalesforceUser;
 import com.revature.caliber.services.TrainingService;
@@ -24,7 +27,7 @@ import com.revature.caliber.services.TrainingService;
  *
  */
 @RestController
-@RequestMapping(produces=MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value="training", produces=MediaType.APPLICATION_JSON_VALUE)
 public class TrainingController {
 
 	private static Logger log = Logger.getLogger(TrainingController.class);
@@ -35,26 +38,19 @@ public class TrainingController {
 		this.trainingService = trainingService;
 	}
 
-	@RequestMapping(value="training/trainer/byemail/{email}", method=RequestMethod.GET)
+	@RequestMapping(value="trainer/byemail/{email}", method=RequestMethod.GET)
 	public ResponseEntity<Trainer> getByEmail(@PathVariable String email){
 		log.info("Find trainer by email " + email);
 		Trainer trainer = trainingService.getByEmail(email);
 		return new ResponseEntity<Trainer>(trainer, HttpStatus.OK);
 	}
 	
-	//@PreAuthorize(value = "hasAnyRole('vp,qc')")
-	@PreAuthorize(value = "hasRole('trainer')")
-	@RequestMapping(value="caliber/trainer/batch/all", method=RequestMethod.GET)
-	public ResponseEntity<Trainer> get(@AuthenticationPrincipal SalesforceUser user){
-		log.info("Testing SecurityContext " + user);
-		return new ResponseEntity<Trainer>(new Trainer(), HttpStatus.OK);
-	}
-	
-	//@PreAuthorize(value = "hasAnyRole('vp,qc')")
-	@PreAuthorize(value = "hasRole('trainer')")
-	@RequestMapping(value="caliber/qc/batch/all", method=RequestMethod.GET)
-	public ResponseEntity<Trainer> getQC(@AuthenticationPrincipal SalesforceUser user){
-		log.info("Testing PreAuthorize and SecurityContext " + user);
-		return new ResponseEntity<Trainer>(new Trainer(), HttpStatus.OK);
+	@RequestMapping(value="trainer/batch/all", method=RequestMethod.GET)
+	public ResponseEntity<List<Batch>> getAllTrainerBatches(Authentication auth){
+		// access user details through SecurityContext 
+		SalesforceUser userPrincipal = (SalesforceUser) auth.getPrincipal();
+		log.debug("Getting all batches for trainerid:" + userPrincipal.getCaliberId() + 
+				" with email " + userPrincipal.getEmail() + " and role " + userPrincipal.getRole());
+		return new ResponseEntity<List<Batch>>(new ArrayList<Batch>(), HttpStatus.OK);
 	}
 }
