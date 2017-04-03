aeddb74a3f0dd997cd372633cfc854b92541c338
Patrick Walsh <patrick.walsh@revature.com>
2017-04-02 20:51:16 -0400

    working on controllers

--- caliber/src/main/java/com/revature/caliber/controllers/ReportingController.java
+++ caliber/src/main/java/com/revature/caliber/controllers/ReportingController.java
@@ -1,18 +1,28 @@
 package com.revature.caliber.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.caliber.beans.Assessment;
+import com.revature.caliber.beans.Batch;
+import com.revature.caliber.beans.Category;
+import com.revature.caliber.beans.Trainee;
 import com.revature.caliber.services.ReportingService;
 
 /**
- * Exclusively used to generate data for charts 
+ * Exclusively used to generate data for charts
+ * 
  * @author Patrick Walsh
  *
  */
@@ -23,7 +33,101 @@ public class ReportingController {
 	private ReportingService reportingService;
 
 	@Autowired
-	public void setReportingService(ReportingService reportingService) {this.reportingService = reportingService;}
+	public void setReportingService(ReportingService reportingService) {
+		this.reportingService = reportingService;
+	}
+
+	/*
+	 *******************************************************
+	 * TODO ORGANIZE SERVICES
+	 *
+	 *******************************************************
+	 */
 	
-	
+	/**
+	 * Get aggregated grades by Category for a Trainee
+	 *
+	 * @param traineeId
+	 * @return
+	 */
+	@RequestMapping(value = "/agg/tech/trainee/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<HashMap<Trainee, Double[]>> aggregateTechTrainee(@PathVariable("id") int traineeId) {
+		// TODO implement me
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	/**
+	 * Get aggregated grades by Week for a Trainee
+	 *
+	 * @param traineeId
+	 * @return
+	 */
+	@RequestMapping(value = "/agg/week/trainee/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<HashMap<Trainee, Double[]>> aggregateWeekTrainee(@PathVariable("id") int traineeId) {
+		// TODO implement me
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	/**
+	 * Get aggregated grades by Category for a Batch
+	 *
+	 * @param batchId
+	 * @return
+	 */
+	@RequestMapping(value = "/agg/tech/batch/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<HashMap<Batch, Double[]>> aggregateTechBatch(@PathVariable("id") int batchId) {
+		// TODO implement me
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	/**
+	 * Get aggregated grades by Category for a Batch
+	 *
+	 * @param traineeId
+	 * @return
+	 */
+	@RequestMapping(value = "/agg/week/batch/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<HashMap<Batch, Double[]>> aggregateWeekBatch(@PathVariable("id") int batchId) {
+		// TODO implement me
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	/**
+	 * Get aggregated grades for all Trainees by Trainer
+	 *
+	 * @param traineeId
+	 * @return
+	 */
+	@RequestMapping(value = "/agg/batch/trainer/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Map<String, Double[]>> aggregateTraineesTrainer(@PathVariable("id") int trainerId) {
+		// TODO implement me
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	public ResponseEntity<Map<Trainee, Double>> findAvgGradesForEachTrainee() {
+		// TODO implement me
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	public ResponseEntity<Map<Assessment, Double>> findAvgGradesForEachAssessment() {
+		// TODO implement me
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	public ResponseEntity<Map<Category, Double>> findAvgGradeByCategory(int traineeId) {
+		// TODO implement me
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	public ResponseEntity<Map<Integer, Double>> findAvgGradeByWeek(int traineeId) {
+		// TODO implement me
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	@RequestMapping(value = "/agg/tech/batch/all", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<HashMap<Category, HashMap<Batch, Double[]>>> aggregateTechForAllBatches() {
+		// TODO implement me
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
 }
--- caliber/src/main/java/com/revature/caliber/services/ReportingService.java
+++ caliber/src/main/java/com/revature/caliber/services/ReportingService.java
@@ -1,11 +1,24 @@
 package com.revature.caliber.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.revature.caliber.beans.Assessment;
+import com.revature.caliber.beans.Batch;
+import com.revature.caliber.beans.Category;
+import com.revature.caliber.beans.Trainee;
 import com.revature.caliber.data.GradeDAO;
-import com.revature.caliber.intel.ClassroomStatisticalAnalysisAlgorithmBean;
+import com.revature.caliber.intel.ClassroomStatisticalAnalysisBean;
 
 /**
  * Exclusively used to generate data for charts
@@ -18,7 +31,7 @@ import com.revature.caliber.intel.ClassroomStatisticalAnalysisAlgorithmBean;
  *
  */
 @Service
-public class ReportingService //implements ClassroomStatisticalAnalysisAlgorithmBean
+public class ReportingService //implements ClassroomStatisticalAnalysisBean
 {
 	private final static Logger log = Logger.getLogger(ReportingService.class);
 	private GradeDAO gradeDAO;
@@ -28,4 +41,79 @@ public class ReportingService //implements ClassroomStatisticalAnalysisAlgorithm
 		this.gradeDAO = gradeDAO;
 	}
 
+	/**
+	 * Get aggregated grades by Category for a Trainee
+	 *
+	 * @param traineeId
+	 * @return
+	 */
+	public HashMap<Trainee, Double[]> aggregateTechTrainee(@PathVariable("id") int traineeId) {
+		// TODO implement me
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	/**
+	 * Get aggregated grades by Week for a Trainee
+	 *
+	 * @param traineeId
+	 * @return
+	 */
+
+	public HashMap<Trainee, Double[]> aggregateWeekTrainee(int traineeId) {
+		// TODO implement me
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	/**
+	 * Get aggregated grades by Category for a Batch
+	 *
+	 * @param batchId
+	 * @return
+	 */
+	public HashMap<Batch, Double[]> aggregateTechBatch(int batchId) {
+		// TODO implement me
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	/**
+	 * Get aggregated grades by Category for a Batch
+	 *
+	 * @param traineeId
+	 * @return
+	 */
+	public HashMap<Batch, Double[]> aggregateWeekBatch(int batchId) {
+		// TODO implement me
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	/**
+	 * Get aggregated grades for all Trainees by Trainer
+	 *
+	 * @param traineeId
+	 * @return
+	 */
+	public Map<String, Double[]> aggregateTraineesTrainer(int trainerId) {
+		// TODO implement me
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	public Map<Trainee, Double> findAvgGradesForEachTrainee() {
+		// TODO implement me
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	public Map<Assessment, Double> findAvgGradesForEachAssessment() {
+		// TODO implement me
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	public Map<Category, Double> findAvgGradeByCategory(int traineeId) {
+		// TODO implement me
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	public Map<Integer, Double> findAvgGradeByWeek(int traineeId) {
+		// TODO implement me
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
 }
