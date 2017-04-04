3f096170bd38e7cd4b83d9b39faf5059218ededb
Igor Gluskin <igluskin94@gmail.com>
2017-04-04 12:28:38 -0400

    updating TraineeDAO and finished Chart2 PieChart (Igor)

--- caliber/src/main/java/com/revature/caliber/services/ReportingService.java
+++ caliber/src/main/java/com/revature/caliber/services/ReportingService.java
@@ -1,14 +1,18 @@
 package com.revature.caliber.services;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.revature.caliber.beans.QCStatus;
 import com.revature.caliber.beans.Batch;
+import com.revature.caliber.beans.Note;
+import com.revature.caliber.beans.NoteType;
+import com.revature.caliber.beans.QCStatus;
+import com.revature.caliber.beans.Trainee;
 import com.revature.caliber.data.AssessmentDAO;
 import com.revature.caliber.data.BatchDAO;
 import com.revature.caliber.data.GradeDAO;
@@ -72,10 +76,30 @@ public class ReportingService
 	}
 	
 
-	public Map<QCStatus, Integer> batchWeekPieChart(){
+	//Pie chart displaying number of trainees that recieved red, yellow, green, or superstar
+	//returns Map relating the number of trainees per QCStatus
+	public Map<QCStatus, Integer> batchWeekPieChart(Integer batchId, Integer weekNumber){
 		
-		
-		return null;
+		List<Trainee> trainees = traineeDAO.findAllByBatch(batchId);
+		Map<QCStatus, Integer> results = new HashMap<>();
+		 for(QCStatus s : QCStatus.values()){
+			 results.put(s, 0);
+		 }
+		 for (Trainee t : trainees){
+			 for (Note n : t.getNotes()){
+				 if (n.getWeek()==weekNumber){
+					 if (n.getType()==NoteType.QC_TRAINEE){
+						 QCStatus status = n.getQcStatus();
+						 Integer temp = results.get(status);
+						 temp++;
+						 results.remove(status);
+						 results.put(status, temp);
+					 }
+				 }
+			 }
+		 }
+		 
+		return results;
 	}
 
 	
