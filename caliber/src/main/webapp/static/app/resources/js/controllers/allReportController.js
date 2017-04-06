bd74dc7bccb933898b0958a31a636cf77c3c8c99
pjw6193 <patrick@revature.com>
2017-04-06 17:28:14 -0400

    Update allReportController.js

--- caliber/src/main/webapp/static/app/resources/js/controllers/allReportController.js
+++ caliber/src/main/webapp/static/app/resources/js/controllers/allReportController.js
@@ -1,6 +1,6 @@
 angular.module("reports").controller("allReportController",
 		function($scope, $log, caliberDelegate, chartsDelegate, allBatches) {
 
-			console.log("Booted Report Controller");
-			console.log("Peacepapi is here!!!!!");
-		})
+			$log.debug("Booted Report Controller");
+			$log.debug("Peacepapi is here!!!!!");
+		});
