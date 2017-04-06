7869a7e6fcaa3bd56db4cf48e439302b080a9ca0
Igor Gluskin <igluskin94@gmail.com>
2017-04-06 15:37:33 -0400

    reportFactory methods for charts 2,3,11

--- caliber/src/main/webapp/static/app/resources/js/factories/reportFactory.js
+++ caliber/src/main/webapp/static/app/resources/js/factories/reportFactory.js
@@ -1,19 +1,59 @@
 angular.module("api").factory("reportFactory", function($http, $log) {
 	$log.debug("Booted Report Factory");
-	
+
 	var report = {};
-//	example
-//	report.techTrainee = function(traineeId) {
-//		return $http({
-//			url : "/all/tech/trainee/" + traineeId,
-//			method : "GET"
-//		}).then(function(response) {
-//			$log.debug("Agg - Tech - trainee -- success");
-//			$log.debug(response);
-//			return response.data;
-//		}, function(response) {
-//			$log.error("There was an error: " + response.status);
-//		});
-//
-//	};
+
+	report.batchWeekQCPie = function(batchId, weekNum) {
+		return $http({
+			url : "all/reports/batch/"+batchId+"/week/"+weekNum+"/pie",
+			method : "GET"
+		}).then(function(response) {
+			$log.debug("Reports - BatchWeekPie -- success");
+			$log.debug(response);
+			return response.data;
+		}, function(response) {
+			$log.error("There was an error: " + response.status);
+		});
+	};
+	
+	report.batchWeekTraineeAssessBar = function(batchId, weekNum, traineeId) {
+		return $http({
+			url : "all/reports/batch/"+batchId+"/week/"+weekNum+"/trainee/"+traineeId+"/bar-batch-week-trainee",
+			method : "GET"
+		}).then(function(response) {
+			$log.debug("Reports - batchWeekTraineeAssessBar -- success");
+			$log.debug(response);
+			return response.data;
+		}, function(response) {
+			$log.error("There was an error: " + response.status);
+		});
+	};
+	
+	report.batchOverallTraineeAssessBar = function(batchId, traineeId) {
+		return $http({
+			url : "all/reports/batch/"+batchId+"/overall/trainee/"+traineeId+"/bar-batch-overall-trainee",
+			method : "GET"
+		}).then(function(response) {
+			$log.debug("Reports - batchWeekTraineeAssessBar -- success");
+			$log.debug(response);
+			return response.data;
+		}, function(response) {
+			$log.error("There was an error: " + response.status);
+		});
+	};
+
+	// example
+	// report.techTrainee = function(traineeId) {
+	// return $http({
+	// url : "/all/tech/trainee/" + traineeId,
+	// method : "GET"
+	// }).then(function(response) {
+	// $log.debug("Agg - Tech - trainee -- success");
+	// $log.debug(response);
+	// return response.data;
+	// }, function(response) {
+	// $log.error("There was an error: " + response.status);
+	// });
+	//
+	// };
 })
