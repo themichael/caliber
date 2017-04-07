03c07897c7a64ba1bca7f00adbc7d4b15fb9bc4c
Igor Gluskin <igluskin94@gmail.com>
2017-04-07 11:29:24 -0400

    sorry, one more change!

--- caliber/src/main/webapp/static/app/resources/js/factories/chartsDelegateFactory.js
+++ caliber/src/main/webapp/static/app/resources/js/factories/chartsDelegateFactory.js
@@ -22,27 +22,14 @@ angular
 					delegate.line = {};
 
 					/**
-					 * ********************* Horizontal Bar
+					 * ********************* Bar
 					 * *********************
 					 */
 					delegate.bar.getBatchAvgChart = function(dataArray) {
 						return barChartFactory.getBatchAvgChart(dataArray);
 					};
 
-					delegate.bar.getTrainerEvalChart = function(dataArray) {
-						return barChartFactory.getTrainerEvalChart(dataArray);
-					};
-
-					delegate.bar.getAllBatchesEvalChart = function(dataArray) {
-						return barChartFactory
-								.getAllBatchesEvalChart(dataArray);
-					};
-
-					delegate.bar.getBatchTechEvalChart = function(dataArray) {
-						return barChartFactory
-								.getBatchTechEvalChart(dataArray);
-					};
-
+				
 					/**
 					 * ************************** Radar
 					 * *************************
@@ -51,34 +38,28 @@ angular
 							dataArray) {
 						return radarChartFactory
 								.getBatchRankComparisonChart(dataArray);
-					};
-
-					delegate.radar.getTraineeTechProgressChart = function(
-							dataArray) {
-						return radarChartFactory
-								.getTraineeTechProgressChart(dataArray);
-					};
-
-					delegate.radar.getAllBatchRankComparisonChart = function(
-							dataArray1, dataArray2) {
-						return radarChartFactory
-								.getAllBatchRankComparisonChart(dataArray1,
-										dataArray2);
-					};
+					
 
 					/**
 					 * ************************** Line
 					 * **************************
 					 */
-					delegate.line.getTraineeProgressChart = function(dataArray) {
-						return lineChartFactory
-								.getTraineeProgressChart(dataArray);
-					};
-
+						
 					delegate.line.getBatchProgressChart = function(dataArray) {
 						return lineChartFactory
 								.getBatchProgressChart(dataArray);
 					};
 
+					/**
+					 * ************************** Doughnut
+					 * **************************
+					 */
+						
+					delegate.doughnut.getBatchProgressChart = function(dataArray) {
+						return lineChartFactory
+								.getBatchProgressChart(dataArray);
+					};
+					
+					
 					return delegate;
 				});
