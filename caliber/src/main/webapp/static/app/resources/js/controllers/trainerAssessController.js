7198b6a0c79cf88e93aafb691cf349013b846eb1
Jack <son.jack0218@gmail.com>
2017-04-07 09:42:39 -0400

    updating

--- caliber/src/main/webapp/static/app/resources/js/controllers/trainerAssessController.js
+++ caliber/src/main/webapp/static/app/resources/js/controllers/trainerAssessController.js
@@ -327,6 +327,9 @@ angular
 						}
 						
 					}
+					$scope.setTraineeModel = function(traineeId){
+						return $scope.trainee[traineeId];
+					}
 					$scope.findGrade = function(traineeId, assessmentId) {
 							if($scope && $scope.grades && ($scope.grades[traineeId] === undefined)){ 
 								return;
