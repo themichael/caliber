beeb5ec3121d380c0eed3735653cce65d94a44a5
Ateeb Khawaja <Ateeb Khawaja>
2017-04-06 22:57:13 -0400

    adding some factory methods

--- caliber/src/main/webapp/static/app/resources/js/factories/reportFactory.js
+++ caliber/src/main/webapp/static/app/resources/js/factories/reportFactory.js
@@ -135,7 +135,65 @@ angular
 											$log.error("There was an error: "
 													+ response.status);
 										}); // end then
-					}
+					};
+
+					report.getTraineeUpToWeekRadarChart = function(week,
+							traineeId) {
+						return $http(
+								{
+									url : "/all/reports/week/" + week
+											+ "/trainee/" + traineeId
+											+ "/radar-trainee-up-to-week",
+									method : "GET"
+								})
+								.then(
+										function(response) {
+											$log
+													.debug("Week - Trainee Up To Week Radar Chart");
+											$log.debug(response);
+											return response.data;
+										},
+										function(response) {
+											$log.error("There was an error: "
+													+ response.status);
+										}); // end then
+					};
+
+					report.getTraineeOverallRadarChart = function(traineeId) {
+						return $http(
+								{
+									url : "/all/reports/trainee/" + traineeId
+											+ "/radar-trainee-overall",
+									method : "GET"
+								}).then(
+								function(response) {
+									$log.debug("Trainee Overall Radar Chart");
+									$log.debug(response);
+									return response.data;
+								},
+								function(response) {
+									$log.error("There was an error: "
+											+ response.status);
+								}); // end then
+					};
+
+					report.getBatchOverallRadarChart = function(batchId) {
+						return $http(
+								{
+									url : "/all/reports/batch/" + batchId
+											+ "/overall/radar-batch-overall",
+									method : "GET"
+								}).then(
+								function(response) {
+									$log.debug("Batch Overall Radar Chart");
+									$log.debug(response);
+									return response.data;
+								},
+								function(response) {
+									$log.error("There was an error: "
+											+ response.status);
+								}); // end then
+					};
 
 					return report;
 
