544cdfbc914387d8b3616879a1c0f06ce8369b98
Ateeb Khawaja <Ateeb Khawaja>
2017-04-10 15:01:31 -0400

    using the refactored factories

--- caliber/src/main/webapp/static/app/resources/js/factories/chartFactories/radarChartFactory.js
+++ caliber/src/main/webapp/static/app/resources/js/factories/chartFactories/radarChartFactory.js
@@ -10,81 +10,37 @@ angular.module("charts").factory("radarChartFactory", function($log) {
 
 	radar.getBatchAvgChart = function(dataArray) {
 		var chartData = {};
-
-		// data and labels
+		
+		chartData.series = [ "Batch" ];
+		
 		chartData.data = [];
 		chartData.labels = [];
+		chartData.data.push([]);
 
-		// traverse through array of objects and grab labels and data
-		dataArray.forEach(function(element) {
-			chartData.labels.push(element.trainee);
-			chartData.data.push(element.average);
-		});
-
-		chartData.datasetOverride = [ {
-			xAxisID : 'x-axis-1'
-		} ];
-
-		return chartData;
-	};
-
-	radar.getTrainerEvalChart = function(dataArray) {
-		var chartData = {};
-
-		// series
-		chartData.series = [ "QC Eval" ];
-
-		// labels and data
-		chartData.data = [];
-		chartData.labels = [];
-
-		// loop through object array
-		dataArray.forEach(function(element) {
-			chartData.data.push(element.score);
-			chartData.labels.push(element.name);
-		});
-
-		return chartData;
-	};
-
-	radar.getAllBatchesEvalChart = function(data, batches) {
-		var chartData = {};
-
-		// series
-		chartData.series = [ "All Batch Eval" ];
-
-		// labels and data
-		chartData.data = [];
-		chartData.labels = [];
-
-		// loop through object array
 		angular.forEach(data, function(value, key) {
-			$log.debug(value);
-			chartData.data.push(value[0]);
-			$log.debug(key);
 			chartData.labels.push(key);
+			chartData.data[0].push(value);
 		});
-
+		
+		chartData.options = radarOptions;
 		return chartData;
 	};
 
-	radar.getBatchTechEvalChart = function(dataArray) {
-		var chartData = {};
+	radarOptions = {
+		legend : {
+			display : true,
+			position : 'bottom'
+		},
+		scale : {
+			reverse : false,
+			ticks : {
+				beginAtZero : false,
+				fixedStepSize : 5,
+				max : 100,
+				suggestedMin : 40
+			}
 
-		// series
-		chartData.series = [ "Tech Batch Eval" ];
-
-		// labels and data
-		chartData.data = [];
-		chartData.labels = [];
-
-		// loop through object array
-		dataArray.forEach(function(element) {
-			chartData.data.push(element.average);
-			chartData.labels.push(element.trainee);
-		});
-
-		return chartData;
+		}
 	};
 
 	return radar;
