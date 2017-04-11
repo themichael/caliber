345319ddaeb6f27cb710411e3f07c22346901487
Hendy Valcin <Hvalcin.grady@gmail.com>
2017-04-11 11:52:44 -0400

    Not keeping these changes

--- caliber/src/main/webapp/static/app/resources/js/controllers/trainerManageController.js
+++ caliber/src/main/webapp/static/app/resources/js/controllers/trainerManageController.js
@@ -129,78 +129,7 @@ angular
 						$scope.row = index;
 					};
 
-					/** Get trainee info* */
-					$scope.getTrainee = function(trainee) {
-						// TODO: MAKE EDIT BUTTON VISABLE AND INVISBLE WHEN
-						// FINISHED
-
-						$scope.editTrainee = trainee;
-						$scope.Updating = true;
-
-					}
-
-					$scope.Updating = false;
-					$scope.updateTrainee = function(editedTrainee) {
-
-						$log.log(editedTrainee);
-						for (var i = 0; i < $scope.receivers.length; i++) {
-
-							if ($scope.receivers[i].name == "") {
-								$scope.receivers[i].name = editedTrainee.name;
-							}
-
-							if ($scope.receivers[i].email == "") {
-								$scope.receivers[i].email = editedTrainee.email;
-							}
-
-							if ($scope.receivers[i].trainingStatus == "") {
-								$scope.receivers[i].trainingStatus = editedTrainee.trainingStatus;
-							}
-
-							if ($scope.receivers[i].phoneNumber == "") {
-								$scope.receivers[i].phoneNumber = editedTrainee.phoneNumber;
-							}
-
-							if ($scope.receivers[i].skypeId == "") {
-								$scope.receivers[i].skypeId = editedTrainee.skypeId;
-							}
-
-							if ($scope.receivers[i].profileUrl == "") {
-								$scope.receivers[i].profileUrl = editedTrainee.profileUrl;
-							}
-							var updTrainee = {
-								traineeId : editedTrainee.traineeId,
-								name : $scope.receivers[i].name,
-								email : $scope.receivers[i].email,
-								trainingStatus : $scope.receivers[i].trainingStatus,
-								phoneNumber : $scope.receivers[i].phoneNumber,
-								skypeId : $scope.receivers[i].skypeId,
-								profileUrl : $scope.receivers[i].profileUrl,
-								batch : $scope.currentBatch
-							};
-
-						}
-						$log.log(updTrainee);
-						editedTrainee = updTrainee;
-						$log.debug(editedTrainee);
-						caliberDelegate.all
-								.updateTrainee(editedTrainee)
-								.then(
-										$scope.clear = function(editedTrainee) {
-											$scope.editTrainee.name = "";
-											$scope.editTrainee.email = "";
-											$scope.editTrainee.phoneNumber = "";
-											$scope.editTrainee.skypeId = "";
-											$scope.editTrainee.profileUrl = "";
-											$scope.Updating = false;
-
-											for (var i = 0; i < $scope.receivers.length; i++) {
-												$scope.receivers[i] = null;
-											}
-
-										});
-					};
-
+					
 					/** Fill update form with batch previous data* */
 					$scope.populateBatch = function(batch) {
 						$scope.batchFormName = "Update Batch";
@@ -474,6 +403,83 @@ angular
 											}
 										})
 
+					};
+
+					/** Get trainee info* */
+					$scope.getTrainee = function(trainee) {
+						// TODO: MAKE EDIT BUTTON VISABLE AND INVISBLE WHEN
+						// FINISHED
+
+						$scope.editTrainee = trainee;
+						$scope.Updating = true;
+
+					}
+
+					$scope.Updating = false;
+					$scope.updateTrainee = function(editedTrainee) {
+
+						$log.log(editedTrainee);
+						for (var i = 0; i < $scope.receivers.length; i++) 
+						{
+
+							if ($scope.receivers[i] == null) {
+								$scope.receivers[i] = editedTrainee;
+							}
+
+							if ($scope.receivers[i].name == "") {
+								$scope.receivers[i].name = editedTrainee.name;
+							}
+
+							if ($scope.receivers[i].email == "") {
+								$scope.receivers[i].email = editedTrainee.email;
+							}
+
+							if ($scope.receivers[i].trainingStatus == "") {
+								$scope.receivers[i].trainingStatus = editedTrainee.trainingStatus;
+							}
+
+							if ($scope.receivers[i].phoneNumber == "") {
+								$scope.receivers[i].phoneNumber = editedTrainee.phoneNumber;
+							}
+
+							if ($scope.receivers[i].skypeId == "") {
+								$scope.receivers[i].skypeId = editedTrainee.skypeId;
+							}
+
+							if ($scope.receivers[i].profileUrl == "") {
+								$scope.receivers[i].profileUrl = editedTrainee.profileUrl;
+							}
+							var updTrainee = {
+								traineeId : editedTrainee.traineeId,
+								name : $scope.receivers[i].name,
+								email : $scope.receivers[i].email,
+								trainingStatus : $scope.receivers[i].trainingStatus,
+								phoneNumber : $scope.receivers[i].phoneNumber,
+								skypeId : $scope.receivers[i].skypeId,
+								profileUrl : $scope.receivers[i].profileUrl,
+								batch : $scope.currentBatch
+							};
+
+						}
+						$log.log(updTrainee);
+						editedTrainee = updTrainee;
+						$log.debug(editedTrainee);
+						caliberDelegate.all
+								.updateTrainee(editedTrainee)
+								.then(
+										$scope.clear = function(editedTrainee) {
+											$scope.editTrainee.name = "";
+											$scope.editTrainee.email = "";
+											$scope.editTrainee.phoneNumber = "";
+											$scope.editTrainee.skypeId = "";
+											$scope.editTrainee.profileUrl = "";
+											$scope.Updating = false;
+
+											for (var i = 0; i < $scope.receivers.length; i++) {
+												$scope.receivers[i] = null;
+											}
+
+										});
 					};
 
 				});
