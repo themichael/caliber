036e3f28acc32cfa63ab431fe98f6c6a6c8b25e9
danliu277 <danliu277@gmail.com>
2017-04-19 09:24:51 -0400

    Added timeout to module so it works the first time you click on the dropdown in reports

--- caliber/src/main/webapp/static/app/resources/js/config/modules.js
+++ caliber/src/main/webapp/static/app/resources/js/config/modules.js
@@ -1,12 +1,18 @@
 /** modules * */
-angular.module("app", [ "ui.router", "ngCookies", "auth", "chart.js", "vp",
-		"qc", "trainer", "api", "delegate", "charts", "reportApi","ngAnimate"]).run(function($rootScope){
-			$rootScope.$on('test',function(){
+angular.module(
+		"app",
+		[ "ui.router", "ngCookies", "auth", "chart.js", "vp", "qc", "trainer",
+				"api", "delegate", "charts", "reportApi", "ngAnimate" ]).run(
+		function($rootScope, $timeout) {
+			$rootScope.$on('test', function() {
 				$rootScope.$broadcast('trainerasses');
 				$rootScope.$broadcast('qcBatchOverall');
 			});
-			$rootScope.$on('GET_TRAINEE_OVERALL',function(event,traineeId){
-				$rootScope.$broadcast('GET_TRAINEE_OVERALL_CTRL', traineeId);
+			$rootScope.$on('GET_TRAINEE_OVERALL', function(event, traineeId) {
+				$timeout(function() {
+					$rootScope
+							.$broadcast('GET_TRAINEE_OVERALL_CTRL', traineeId)
+				}, 100);
 			})
 		})
 // add modules here
