057d115df67e5edbdbe8e807d13e85ac4306671d
Ateeb Khawaja <ateebtahir@gmail.com>
2017-04-20 11:32:38 -0400

    finished stacked bar for qc stats

--- caliber/src/main/webapp/static/app/resources/js/factories/chartFactories/barChartFactory.js
+++ caliber/src/main/webapp/static/app/resources/js/factories/chartFactories/barChartFactory.js
@@ -250,36 +250,46 @@ angular
 						chartData.series = [];
 						chartData.data = [];
 						chartData.labels = [];
-			
-						angular.forEach(data, function(value, key) {
-							angular.forEach(value, function(value2, key2) {
-								// Push in an array for each QC Status
-								chartData.data.push([]);
-							});
-						});
+						chartData.colors = [];
 						
-						
-						var labelsTemp = [];
-						var i = 0;
+						var len = chartData.data.length;
 						angular.forEach(data, function(value, key) {
+							chartData.labels.push(key);
+							var i = 0;
 							angular.forEach(value, function(value2, key2) {
+								if(chartData.data[i] === undefined){
+									chartData.data.push([]);
+									chartData.series.push(key2);
+									if(key2 === "Superstar") chartData.colors.push("#7972ff");
+									else if( key2 === "Good") chartData.colors.push("#81f575");
+									else if( key2 === "Average") chartData.colors.push("#e8b00b");
+									else if( key2 === "Poor") chartData.colors.push("#ff7575");
+								}
 								chartData.data[i].push(value2);
 								i++;
 							});
-							i = 0;
+							
 						});
 						
-						chartData.labels = labelsTemp;
-						
-						chartData.colors = [mainColor];
 						chartData.options = {
+							legend : {
+								display : true,
+								labels : {
+									boxWidth : 10
+								}
+							},
 							scales : {
 								yAxes : [ {
 									stacked: true,
-									ticks: {mirror: true}
+									ticks: {
+										mirror: true
+									}
 								} ],
 								xAxes: [{ 
-						          	stacked: true
+						          	stacked: true,
+						          	ticks: {
+						          		mirror: true
+						          	}
 								}]
 							}
 						}
