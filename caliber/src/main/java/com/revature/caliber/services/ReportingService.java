715d0562b824e77fd46ef465dac826e01d33f6f5
Igor Gluskin <igluskin94@gmail.com>
2017-04-21 14:50:03 -0400

    all current batches line chart

--- caliber/src/main/java/com/revature/caliber/services/ReportingService.java
+++ caliber/src/main/java/com/revature/caliber/services/ReportingService.java
@@ -358,11 +358,12 @@ public class ReportingService {
 	}
 	
 	public Map<String, Map<Integer, Double>> getAllCurrentBatchesLineChart(){
-		Map<String, Map<Integer, Double>> results = new ConcurrentHashMap<>();
-		
-		
-		
-		
+		Map<String, Map<Integer, Double>> results = new HashMap<>();
+		List<Batch> batches = batchDAO.findAllCurrent();
+		for(Batch batch : batches){
+			List<Trainee> trainees = new ArrayList<>(batch.getTrainees());
+			results.put(batch.getTrainingName(), utilAvgBatchOverall(trainees, batch.getWeeks()));
+		}
 		return results;
 	}
 
