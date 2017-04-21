b4a77528f62c2b123ede066729bb690e64ca311e
Igor Gluskin <igluskin94@gmail.com>
2017-04-21 17:13:59 -0400

    pushing line chart

--- caliber/src/main/webapp/static/app/resources/js/controllers/vpHomeController.js
+++ caliber/src/main/webapp/static/app/resources/js/controllers/vpHomeController.js
@@ -44,19 +44,19 @@ angular
 										});
 					}
 					function createCurrentBatchesAverageScoreChart() {
-						chartsDelegate.bar.data
-								.getAllBatchesCurrentWeekQCStatsData()
+						chartsDelegate.line.data
+								.getCurrentBatchesAverageScoreChartData()
 								.then(
 										function(data) {
 											NProgress.done();
-											var barChartObj = chartsDelegate.bar
-													.getAllBatchesCurrentWeekQCStats(data);
+											var lineChartObj = chartsDelegate.line
+													.getCurrentBatchesAverageScoreChart(data);
 
-											$scope.stackedBarData = barChartObj.data;
-											$scope.stackedBarLabels = barChartObj.labels;
-											$scope.stackedBarSeries = barChartObj.series;
-											$scope.stackedBarOptions = barChartObj.options;
-											$scope.stackedBarColors = barChartObj.colors;
+											$scope.currentBatchesLineData = lineChartObj.data;
+											$scope.currentBatchesLineLabels = lineChartObj.labels;
+											$scope.currentBatchesLineSeries = lineChartObj.series;
+											$scope.currentBatchesLineOptions = lineChartObj.options;
+											$scope.currentBatchesLineColors = lineChartObj.colors;
 
 										}, function() {
 											NProgress.done();
