03b48f85b6663d58ec8e98d8561662427929d68a
Patrick Walsh <patrick.walsh@revature.com>
2017-04-25 11:23:27 -0400

    pdf cleanup

--- caliber/src/main/java/com/revature/caliber/controllers/BootController.java
+++ caliber/src/main/java/com/revature/caliber/controllers/BootController.java
@@ -74,7 +74,7 @@ public class BootController extends Helper {
 	 * @throws URISyntaxException
 	 *             the uri syntax exception
 	 */
-	//@RequestMapping(value = "/caliber")
+	@RequestMapping(value = "/caliber")
 	public String devHomePage(HttpServletRequest servletRequest, HttpServletResponse servletResponse)
 			throws IOException, URISyntaxException {
 		// fake Salesforce User
@@ -134,7 +134,7 @@ public class BootController extends Helper {
 	 * @throws URISyntaxException
 	 *             the uri syntax exception
 	 */
-	@RequestMapping(value = "/caliber")
+	//@RequestMapping(value = "/caliber")
 	public String getHomePage(HttpServletRequest servletRequest, HttpServletResponse servletResponse)
 			throws IOException, URISyntaxException {
 		// get Salesforce token from cookie
