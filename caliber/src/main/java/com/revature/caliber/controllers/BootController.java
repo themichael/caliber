c1ed2a579bfb15cf7c55ccad45ba2b4ee5c7db7a
Stanley Chouloute <chouloute1@gmail.com>
2017-06-28 13:27:20 -0400

    configure dev

--- caliber/src/main/java/com/revature/caliber/controllers/BootController.java
+++ caliber/src/main/java/com/revature/caliber/controllers/BootController.java
@@ -66,7 +66,7 @@ public class BootController extends Helper {
 	 * @throws URISyntaxException
 	 *             the uri syntax exception
 	 */
-	//@RequestMapping(value = "/caliber")
+	@RequestMapping(value = "/caliber")
 	public String devHomePage(HttpServletRequest servletRequest, HttpServletResponse servletResponse)
 			throws IOException, URISyntaxException {
 		HttpClient httpClient = HttpClientBuilder.create().build();
@@ -126,7 +126,7 @@ public class BootController extends Helper {
 	 * @throws URISyntaxException
 	 *             the uri syntax exception
 	 */
-	@RequestMapping(value = "/caliber")
+	//@RequestMapping(value = "/caliber")
 	public String getHomePage(HttpServletRequest servletRequest, HttpServletResponse servletResponse)
 			throws IOException, URISyntaxException {
 		HttpClient httpClient = HttpClientBuilder.create().build();
