40b7a8016ac751f6836f93d2b1dbab7ffe8d3e6f
Alec Michel <alecmichel@gmail.com>
2017-07-05 09:57:03 -0500

    transformation batch package modified

--- caliber/src/main/java/com/revature/caliber/salesforce/SalesforceTransformerToCaliber.java
+++ caliber/src/main/java/com/revature/caliber/salesforce/SalesforceTransformerToCaliber.java
@@ -1,13 +1,18 @@
 package com.revature.caliber.salesforce;
 
+import com.revature.caliber.beans.Batch;
+import com.revature.caliber.beans.Trainer;
+import com.revature.salesforce.beans.BatchTrainer;
+import com.revature.salesforce.beans.SalesforceBatch;
+
 public class SalesforceTransformerToCaliber {
 
 	public void transformId() {
 
 	}
 
-	public void transformName() {
-
+	public void transformName(SalesforceBatch salesforceBatch) {
+		Batch batch = new Batch();
 	}
 
 	public void transformBatchStartDate() {
@@ -18,8 +23,8 @@ public class SalesforceTransformerToCaliber {
 
 	}
 
-	public void transformTrainer() {
-
+	public void transformTrainer(BatchTrainer batchTrainer) {
+		Trainer trainer = new Trainer();		
 	}
 
 	public void transformCotrainer() {
@@ -33,5 +38,6 @@ public class SalesforceTransformerToCaliber {
 	public void transformType() {
 
 	}
+	
 
 }
