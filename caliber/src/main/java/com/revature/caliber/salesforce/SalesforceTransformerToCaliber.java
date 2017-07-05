43c05f8a3d6f2a37c85a6d16b864a314ac43b19a
sanmy <saantoshm@gmail.com>
2017-07-05 14:37:36 -0400

    progress on batch transformer

--- caliber/src/main/java/com/revature/caliber/salesforce/SalesforceTransformerToCaliber.java
+++ caliber/src/main/java/com/revature/caliber/salesforce/SalesforceTransformerToCaliber.java
@@ -1,43 +1,45 @@
 package com.revature.caliber.salesforce;
 
 import com.revature.caliber.beans.Batch;
-import com.revature.caliber.beans.Trainer;
-import com.revature.salesforce.beans.BatchTrainer;
 import com.revature.salesforce.beans.SalesforceBatch;
 
 public class SalesforceTransformerToCaliber {
 
-	public void transformId() {
-
+	public void transformId(SalesforceBatch salesforceBatch) {
+		Batch batch = new Batch();
+		batch.setResourceId(salesforceBatch.getId());
 	}
 
 	public void transformName(SalesforceBatch salesforceBatch) {
 		Batch batch = new Batch();
+		batch.setTrainingName(salesforceBatch.getName());
 	}
 
-	public void transformBatchStartDate() {
-
+	public void transformBatchStartDate(SalesforceBatch salesforceBatch) {
+		Batch batch = new Batch();
+		batch.setStartDate(salesforceBatch.getBatchStartDate());
 	}
 
-	public void transformBatchEndDate() {
-
+	public void transformBatchEndDate(SalesforceBatch salesforceBatch) {
+		Batch batch = new Batch();
+		batch.setEndDate(salesforceBatch.getBatchEndDate());
 	}
 
-	public void transformTrainer(BatchTrainer batchTrainer) {
-		Trainer trainer = new Trainer();		
+	public void transformTrainer(SalesforceBatch salesforceBatch) {
+		Batch batch = new Batch();
 	}
 
-	public void transformCotrainer() {
-
+	public void transformCotrainer(SalesforceBatch salesforceBatch) {
+		Batch batch = new Batch();
 	}
 
-	public void transformSkillType() {
-
+	public void transformSkillType(SalesforceBatch salesforceBatch) {
+		Batch batch = new Batch();
 	}
 
-	public void transformType() {
-
+	public void transformType(SalesforceBatch salesforceBatch) {
+		Batch batch = new Batch();
+		batch.setTrainingType(salesforceBach.getType);
 	}
-	
 
 }
