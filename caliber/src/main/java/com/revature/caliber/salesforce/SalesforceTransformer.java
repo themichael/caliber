db184e732d0261b96ad707d4e99840a7ee17c25b
sanmy <saantoshm@gmail.com>
2017-07-05 10:48:34 -0400

    new package

--- caliber/src/main/java/com/revature/caliber/salesforce/SalesforceTransformer.java
+++
@@ -1,5 +0,0 @@
-package com.revature.caliber.salesforce;
-
-public class SalesforceTransformer {
-
-}
---
+++ caliber/src/main/java/com/revature/caliber/salesforce/SalesforceTransformerToCaliber.java
@@ -0,0 +1,37 @@
+package com.revature.caliber.salesforce;
+
+public class SalesforceTransformerToCaliber {
+
+	public void transformId() {
+
+	}
+
+	public void transformName() {
+
+	}
+
+	public void transformBatchStartDate() {
+
+	}
+
+	public void transformBatchEndDate() {
+
+	}
+
+	public void transformTrainer() {
+
+	}
+
+	public void transformCotrainer() {
+
+	}
+
+	public void transformSkillType() {
+
+	}
+
+	public void transformType() {
+
+	}
+
+}
