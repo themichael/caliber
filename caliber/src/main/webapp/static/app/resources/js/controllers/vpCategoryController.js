7808b691be7c3709284066f578575ea94aeae74c
Humberto <humbertocorea1@gmail.com>
2017-07-07 14:32:04 -0400

    fixed controller issue

--- caliber/src/main/webapp/static/app/resources/js/controllers/vpCategoryController.js
+++ caliber/src/main/webapp/static/app/resources/js/controllers/vpCategoryController.js
@@ -1,31 +1,20 @@
 /*******************************************************************************
- * Team: HKM 
- * Team Lead: Humberto Corea 
- * Authors: Humberto Corea, s
- * Rex Toothman,
+ * Team: HKM Team Lead: Humberto Corea Authors: Humberto Corea, s Rex Toothman,
  * Carlos Vallejo
  * 
- * Humberto worked on adding functionality to deactivate an assessment no longer 
- * 			being used in Reavture
- * Rex worked on adding functionality that allows a vp to add a new assessment
- * Carlos worked on adding functionality to edit and assessment.
- * Humberto and Carlos are working concurrently in the edit assessments to include the
- * 			functionality to be able to toggle an assessment from active to inactive.
+ * Humberto worked on adding functionality to deactivate an assessment no longer
+ * being used in Reavture Rex worked on adding functionality that allows a vp to
+ * add a new assessment Carlos worked on adding functionality to edit and
+ * assessment. Humberto and Carlos are working concurrently in the edit
+ * assessments to include the functionality to be able to toggle an assessment
+ * from active to inactive.
  * ******************************************************************************
  */
 
-angular
-		.module("vp")
-		.controller(
-				"vpCategoryController",
-				function($scope, $log, caiberDelegate) {
-					
-				});
-
-
-
-//angular.module("vp").controller("vpCategoryController",function($scope, $log, caliberDelegate) {
-//	
-//	$scope.GetAllCategories = function(){};
-//	
-//});
+angular.module("vp").controller("vpCategoryController",
+		function($scope, $log, caliberDelegate) {
+			caliberDelegate.vp.getAllCategories().then(function(categories) {
+				$log.debug(categories);
+				$scope.categories = categories;
+			});
+		});
