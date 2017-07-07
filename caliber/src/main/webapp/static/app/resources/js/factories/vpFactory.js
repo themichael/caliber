5b089d76466f95f294427d3768ff88bcbd7137af
Roderick Dye <lawtondye@gmail.com>
2017-07-07 10:16:07 -0400

    merged with v2

--- caliber/src/main/webapp/static/app/resources/js/factories/vpFactory.js
+++ caliber/src/main/webapp/static/app/resources/js/factories/vpFactory.js
@@ -22,6 +22,7 @@ angular.module("api").factory("vpFactory", function($log, $http) {
 			$log.error("There was an error: " + response.status);
 		});
 	};
+	
 	// Get all current batches
 	vp.getAllCurrentBatches = function() {
 		return $http({
@@ -34,8 +35,7 @@ angular.module("api").factory("vpFactory", function($log, $http) {
 			$log.error("There was an error: " + response.status);
 		});
 	};
-<<<<<<< HEAD
-
+	
 	//deactivate trainer needed to force content type to be JSON else 415
 	vp.deactivateTrainer = function (trainerObj){
 		return $http({
@@ -52,8 +52,6 @@ angular.module("api").factory("vpFactory", function($log, $http) {
 			$log.error("There was an error: " + response.status);
 		});
 	};
-
-
 	
 	//Update selected trainer
 	vp.updateTrainer = function(trainerObj) {
@@ -124,20 +122,6 @@ angular.module("api").factory("vpFactory", function($log, $http) {
 			return response;
 		});
 	};
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-=======
->>>>>>> ebc64b20e47976667ab4d1b3cf317f882e7f5e5e
+
 	return vp;
 });
