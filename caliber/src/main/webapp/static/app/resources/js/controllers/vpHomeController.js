2182b4f0951f19ec144289828558b4cb11dc408f
Dc Liao <dcliao92@gmail.com>
2017-08-15 20:38:33 -0400

    select unique states dropdown

--- caliber/src/main/webapp/static/app/resources/js/controllers/vpHomeController.js
+++ caliber/src/main/webapp/static/app/resources/js/controllers/vpHomeController.js
@@ -2,14 +2,18 @@ angular
 		.module("vp")
 		.controller(
 				"vpHomeController",
-				function($scope, $log, caliberDelegate, chartsDelegate) {
+				function($scope, $log, caliberDelegate, chartsDelegate, $filter) {
 					$log.debug("Booted vp home controller.");
+					$scope.averageScoreData = {};
+					$scope.states=[];
+					$scope.cities=[];
+					$scope.onLineCharAddressStateChange = onLineCharAddressStateChange;
 					(function() {
 						// Finishes any left over ajax animation from another
 						// page
 						NProgress.done();
+						getCurrentBatchesAvergeScoreData();
 						createAllBatchesCurrentWeekQCStats();
-						createCurrentBatchesAverageScoreChart();
 					})();
 
 					function createAllBatchesCurrentWeekQCStats() {
@@ -31,26 +35,66 @@ angular
 											NProgress.done();
 										});
 					}
-					function createCurrentBatchesAverageScoreChart() {
-						chartsDelegate.line.data
-								.getCurrentBatchesAverageScoreChartData()
-								.then(
-										function(data) {
-											console.log(data);
-											NProgress.done();
+					function createCurrentBatchesAverageScoreChart(data) {
 											var lineChartObj = chartsDelegate.line
 													.getCurrentBatchesAverageScoreChart(data);
-
 											$scope.currentBatchesLineData = lineChartObj.data;
 											$scope.currentBatchesLineLabels = lineChartObj.labels;
 											$scope.currentBatchesLineSeries = lineChartObj.series;
 											$scope.currentBatchesLineOptions = lineChartObj.options;
 											$scope.currentBatchesLineColors = lineChartObj.colors;
 											$scope.currentBatchesDsOverride = lineChartObj.datasetOverride;
-
-										}, function() {
-											NProgress.done();
-										});
+					}
+					
+					function getCurrentBatchesAvergeScoreData(){
+						chartsDelegate.line.data
+						.getCurrentBatchesAverageScoreChartData()
+						.then(
+								function(data) {
+									$scope.averageScoreData = data;
+									$scope.states = generateStateList(data);
+									createCurrentBatchesAverageScoreChart(data);
+									NProgress.done();
+								},function() {
+									NProgress.done();
+								});
+					}
+					
+					function generateStateList(addressList){
+						var states = [];
+						angular.forEach(addressList, function(value, key){
+							states.push(value.address.state);
+						});
+						let uniqueStates = [...new Set(states)];
+						return uniqueStates;
+					}
+					
+					function generateCityListFromState(addressList, state){
+						var cities = [];
+						angular.forEach(addressList, function(value, key){
+							if(value.address.state == state){
+								cities.push(value.address.city);
+							}
+						});
+						let uniqueCities = [...new Set(cities)];
+						return uniqueCities;
+					}
+					
+					function onLineCharAddressCityChange(city){
+						// TODO filter the addressList to city
+					}
+					
+					function filterAddressByState(){
+						
+					}
+					
+					function onLineCharAddressStateChange(state){
+						if(state!=""){
+							// TODO filter the addressList to state;
+							console.log(state);
+//							$scope.cities = generateCityListFromState($scope.averageScoreData,state);
+							
+						}
 					}
 
 				});
