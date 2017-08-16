29375eed58dba03746941b8f83ceeb37e39f8130
Dc Liao <dcliao92@gmail.com>
2017-08-16 10:19:58 -0400

    fix parsing dashboard data

--- caliber/src/main/webapp/static/app/resources/js/factories/chartFactories/lineChartFactory.js
+++ caliber/src/main/webapp/static/app/resources/js/factories/chartFactories/lineChartFactory.js
@@ -207,7 +207,7 @@ angular.module("charts").factory("lineChartFactory", function($log) {
 			var currentWeek = 1;
 			chartData.series.push(batch.label);
 			var temp = [];
-			angular.forEach(value.grades, function(value2, key2) {
+			angular.forEach(batch.grades, function(value2, key2) {
 				while (currentWeek < key2) {
 					temp.push(0);
 					currentWeek++;
