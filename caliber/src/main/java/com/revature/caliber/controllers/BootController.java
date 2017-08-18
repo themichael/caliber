35cb85f380188fcd894f56f6146422ab2028881a
adavis20 <hotblade33@hotmail.com>
2017-08-18 15:00:29 -0400

    Trying to fix token in URL v2

--- caliber/src/main/java/com/revature/caliber/controllers/BootController.java
+++ caliber/src/main/java/com/revature/caliber/controllers/BootController.java
@@ -24,6 +24,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -71,7 +72,7 @@ public class BootController extends Helper {
 	 *             the uri syntax exception
 	 */
 	@RequestMapping(value = "/caliber")
-	public String devHomePage(HttpServletRequest servletRequest, HttpServletResponse servletResponse,@ModelAttribute("salestoken") String salesTokenString)
+	public String devHomePage(HttpServletRequest servletRequest, HttpServletResponse servletResponse,@ModelAttribute("salestoken") String salesTokenString, Model model)
 			throws IOException, URISyntaxException {
 		if (debug) {
 			// fake Salesforce User
@@ -99,7 +100,8 @@ public class BootController extends Helper {
 
 			// authorize user
 			authorize(jsonString, salesforceUser, servletResponse);
-			return "redirect:/home";
+            model.asMap().clear();
+			return "index";
 		} catch (AuthenticationCredentialsNotFoundException e) {
 			log.error("error thrown:" ,e);
 			return "redirect:/";
