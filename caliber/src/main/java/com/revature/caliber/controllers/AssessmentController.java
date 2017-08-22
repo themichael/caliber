cb1954fa6b9b1047efba13b21fe69a47a22b4052
iRzLi <irzli1995@gmail.com>
2017-08-22 13:22:33 -0400

    test1

--- caliber/src/main/java/com/revature/caliber/controllers/AssessmentController.java
+++ caliber/src/main/java/com/revature/caliber/controllers/AssessmentController.java
@@ -29,7 +29,7 @@ import com.revature.caliber.services.AssessmentService;
  *
  */
 @RestController
-//@PreAuthorize("isAuthenticated()")
+@PreAuthorize("isAuthenticated()")
 public class AssessmentController {
 
 	private static final Logger log = Logger.getLogger(AssessmentController.class);
