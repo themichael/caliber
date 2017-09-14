e93ebef91797fc350c5a4136ba5dbba1b34ffcce
Patrick <patrick.walsh@revature.com>
2017-09-14 10:52:06 -0400

    uat/bdd cukes project

---
+++ cukes/src/test/java/com/revature/caliber/cukes/CukesTest.java
@@ -0,0 +1,10 @@
+package com.revature.caliber.cukes;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features={})
+public class CukesTest {}
