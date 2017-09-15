1c3c7832c88f5accf706ce88957ca53e6a0e166c
ChristiZimmer <christi_zimmer@yahoo.com>
2017-09-15 09:15:07 -0400

    New test package structure

---
+++ caliber/src/test/java/com/revature/caliber/test/unit/GradeDAOTest.java
@@ -0,0 +1,11 @@
+package com.revature.caliber.test.unit;
+
+import org.apache.log4j.Logger;
+
+import com.revature.caliber.CaliberTest;
+
+public class GradeDAOTest extends CaliberTest {
+	
+	private static final Logger log = Logger.getLogger(GradeDAOTest.class);
+
+}
---
+++ caliber/src/test/java/com/revature/caliber/test/unit/NoteDAOTest.java
@@ -0,0 +1,11 @@
+package com.revature.caliber.test.unit;
+
+import org.apache.log4j.Logger;
+
+import com.revature.caliber.CaliberTest;
+
+public class NoteDAOTest extends CaliberTest {
+	
+	private static final Logger log = Logger.getLogger(NoteDAOTest.class);
+
+}
