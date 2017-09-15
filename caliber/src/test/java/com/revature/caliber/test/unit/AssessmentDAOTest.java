b6f7f059e64e898de0a8c7f884ad39d2fea5d90b
Peck Boozer <swordbomber2@gmail.com>
2017-09-15 16:26:10 -0400

    added assessmentId to Assessment toString & update test works

--- caliber/src/test/java/com/revature/caliber/test/unit/AssessmentDAOTest.java
+++ caliber/src/test/java/com/revature/caliber/test/unit/AssessmentDAOTest.java
@@ -20,7 +20,6 @@ public class AssessmentDAOTest extends CaliberTest{
 	private static final Logger log = Logger.getLogger(AssessmentDAOTest.class);
 	
 
-//	private Assessment assessment;
 	@Autowired
 	private AssessmentDAO assessmentDao;
 	private static final String ASSESSMENT_COUNT = "select count(assessment_id) from caliber_assessment";
@@ -29,65 +28,65 @@ public class AssessmentDAOTest extends CaliberTest{
 	 * Tests methods:
 	 * com.revature.caliber.data.AssessmentDAO.save()
 	 */
-//	@Test
-//	public void saveAssessmentTest() {
-//		Assessment assessment = assessmentDao.findAll().get(1);
-//		assessmentDao.save(assessment);
-//		assertEquals(assessment, assessmentDao.findAll().get(1));
-//		}
-//	
-//	
-//	/*
-//	 * Tests methods:
-//	 * com.revature.caliber.data.AssessmentDAO.findOne()
-//	 */
-//	@Test
-//	public void findOneAssessmentTest() {
-//		int assessmentId = 2074;
-//		assessmentDao.findOne(assessmentId);
-//		assertEquals(assessmentId, assessmentDao.findOne(assessmentId).getAssessmentId());
-//		}
-//	
-//	
-//	/*
-//	 * Tests methods:
-//	 * com.revature.caliber.data.AssessmentDAO.findAll()
-//	 */
-//	@Test
-//	public void findAllAssessmentTest() {
-//		assertNotNull(assessmentDao.findAll());
-//		}
-//	
-//	
-//	/*
-//	 * Tests methods:
-//	 * com.revature.caliber.data.AssessmentDAO.findByWeek(batchId, weekNumber)
-//	 */
-//	@Test
-//	public void findAssessmentsByWeekTest() {
-//		int batchId = 2150;
-//		int weekNumber = 3;
-//		assertNotNull(assessmentDao.findByWeek(batchId, weekNumber));
-//		}
-//	
-//	
-//	
-//	/**
-//	 * Tests method:
-//	 * com.revature.caliber.data.AssessmentDAO.findByBatchId(Integer batchId)
-//	 */
-//	@Test
-//	public void findByBatchIdAssessmentDAOTest() {
-//		log.info("FIND BY BATCH ID ASSESSMENT DAO");
-//		Assessment assessment =assessmentDao.findAll().get(0);
-//		List<Assessment> assesments = assessmentDao.findByBatchId(2050);
-//		assertEquals(assessment, assesments.get(0));
-//	}
-//	
-//	/**
-//	 * Tests method:
-//	 * com.revature.caliber.data.AssessmentDAO.update(Assessment assessment)
-//	 */
+	@Test
+	public void saveAssessmentTest() {
+		Assessment assessment = assessmentDao.findAll().get(1);
+		assessmentDao.save(assessment);
+		assertEquals(assessment, assessmentDao.findAll().get(1));
+		}
+	
+	
+	/*
+	 * Tests methods:
+	 * com.revature.caliber.data.AssessmentDAO.findOne()
+	 */
+	@Test
+	public void findOneAssessmentTest() {
+		int assessmentId = 2074;
+		assessmentDao.findOne(assessmentId);
+		assertEquals(assessmentId, assessmentDao.findOne(assessmentId).getAssessmentId());
+		}
+	
+	
+	/*
+	 * Tests methods:
+	 * com.revature.caliber.data.AssessmentDAO.findAll()
+	 */
+	@Test
+	public void findAllAssessmentTest() {
+		assertNotNull(assessmentDao.findAll());
+		}
+	
+	
+	/*
+	 * Tests methods:
+	 * com.revature.caliber.data.AssessmentDAO.findByWeek(batchId, weekNumber)
+	 */
+	@Test
+	public void findAssessmentsByWeekTest() {
+		int batchId = 2150;
+		int weekNumber = 3;
+		assertNotNull(assessmentDao.findByWeek(batchId, weekNumber));
+		}
+	
+	
+	
+	/**
+	 * Tests method:
+	 * com.revature.caliber.data.AssessmentDAO.findByBatchId(Integer batchId)
+	 */
+	@Test
+	public void findByBatchIdAssessmentDAOTest() {
+		log.info("FIND BY BATCH ID ASSESSMENT DAO");
+		Assessment assessment =assessmentDao.findAll().get(0);
+		List<Assessment> assesments = assessmentDao.findByBatchId(2050);
+		assertEquals(assessment, assesments.get(0));
+	}
+	
+	/**
+	 * Tests method:
+	 * com.revature.caliber.data.AssessmentDAO.update(Assessment assessment)
+	 */
 	@Test
 	public void updateAssessmentDAOTest() {
 		log.info("UPDATE ASSESSMENT DAO");
@@ -113,15 +112,6 @@ public class AssessmentDAOTest extends CaliberTest{
 	 * Tests method:
 	 * com.revature.caliber.data.AssessmentDAO.delete(Assessment assessment)
 	 */
-//	@Test
-//	public void deleteAssessmentDAOTest() {
-//		log.info("DELETE ASSESSMENT DAO");
-//		Long beforeTest = jdbcTemplate.queryForObject(ASSESSMENT_COUNT, Long.class);
-//		assessmentDao.delete(assessmentDao.findAll().get(1));
-//		Long afterTest = jdbcTemplate.queryForObject(ASSESSMENT_COUNT, Long.class);
-//		assertEquals(--beforeTest, afterTest);
-//		
-//	}
 	
 }
 
