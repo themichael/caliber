810c864c0ae100482c7d8917bc9886650fcf4314
Patrick <patrick.walsh@revature.com>
2017-09-15 09:10:28 -0400

    example Gherkins

---
+++ caliber/src/test/java/com/revature/caliber/CukesE2E.java
@@ -0,0 +1,12 @@
+package com.revature.caliber;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+
+/*import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features={"src/test/resources/features"})
+@ContextConfiguration(locations = { "classpath:integration-test.xml" })*/
+public class CukesE2E{}
