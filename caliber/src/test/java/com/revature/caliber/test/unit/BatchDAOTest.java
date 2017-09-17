22475822b760657fd74f14c97d4d147c8a02cc4c
William <william.clayton2501@gmail.com>
2017-09-17 15:10:35 -0400

    Worked on BatchDAOTest and fixed on of TrainingTest's test after changing the setup.sql

--- caliber/src/test/java/com/revature/caliber/test/unit/BatchDAOTest.java
+++ caliber/src/test/java/com/revature/caliber/test/unit/BatchDAOTest.java
@@ -1,34 +1,15 @@
 package com.revature.caliber.test.unit;
 
-import org.apache.log4j.Logger;
-import org.hibernate.Criteria;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Order;
-import org.hibernate.criterion.Restrictions;
-import org.hibernate.sql.JoinType;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Answers;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
-
-import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.revature.caliber.CaliberTest;
 import com.revature.caliber.beans.Batch;
-import com.revature.caliber.beans.TrainingStatus;
 import com.revature.caliber.data.BatchDAO;
 
 public class BatchDAOTest extends CaliberTest{
@@ -37,8 +18,6 @@ public class BatchDAOTest extends CaliberTest{
 	
 	@Autowired
 	private BatchDAO batchDAO;
-
-	
 
 	/**
 	 * Tests methods:
@@ -59,7 +38,7 @@ public class BatchDAOTest extends CaliberTest{
 	public void findAllCurrentWithNotesTest(){
 		log.info("Testing the BatchDAO.findAllCurrentWithNotesTest()");
 		List<Batch> batches = batchDAO.findAllCurrentWithNotes();
-		log.info(batches.size());
+		assertEquals(1,batches.size());
 	}
 	
 	/**
@@ -68,7 +47,9 @@ public class BatchDAOTest extends CaliberTest{
 	 */
 	@Test
 	public void findAllCurrentWithNotesAndTraineesTest(){
-		
+		log.info("Testing the BatchDAO.findAllCurrentWithNotesAndTrainees()");
+		List<Batch> batches = batchDAO.findAllCurrentWithNotesAndTrainees();
+		assertEquals(1,batches.size());
 	}
 	
 	/**
@@ -77,6 +58,8 @@ public class BatchDAOTest extends CaliberTest{
 	 */
 	@Test
 	public void findAllCurrentWithTraineesTest(){
-		
+		log.info("Testing the BatchDAO.findAllCurrentWithTrainees()");
+		List<Batch> batches = batchDAO.findAllCurrentWithTrainees();
+		assertEquals(4,batches.size());
 	}
 }
