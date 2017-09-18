29cc8bfc2a59d6a0be099fa2a048478b963e5123
TinfoilxD <TinfoilxD@gmail.com>
2017-09-18 11:06:02 -0400

    tested reportingservices for line charts

--- caliber/src/test/java/com/revature/caliber/test/unit/ReportingServiceTest.java
+++ caliber/src/test/java/com/revature/caliber/test/unit/ReportingServiceTest.java
@@ -14,7 +14,7 @@ import com.revature.caliber.services.ReportingService;
 public class ReportingServiceTest extends CaliberTest{
 
 	private static final int TEST_BATCH_ID = 2150;
-	private static final int TEST_ASSESSMENT_WEEK = 7;
+	private static final int TEST_ASSESSMENT_WEEK = 6;
 	private static final int TEST_TRAINEE_ID = 5460;
 	private static final double FLOATING_NUMBER_VARIANCE = .01;
 
@@ -36,7 +36,9 @@ public class ReportingServiceTest extends CaliberTest{
 	 */
 	@Test
 	public void getTraineeUpToWeekLinechart(){
+		
 		/*
+		 * Method description:
 		 * input: batch, week, and trainee
 		 * output: map of week and scores 
 		 */
@@ -48,15 +50,44 @@ public class ReportingServiceTest extends CaliberTest{
 						TEST_ASSESSMENT_WEEK,
 						TEST_TRAINEE_ID);
 		
-		//size should be 7 for week 7
-		assertEquals(7, averageGrades.size());
+		//size should be 6 for week 6
+		assertEquals(TEST_ASSESSMENT_WEEK, averageGrades.size());
 		
 		//equal to data from database
 		assertEquals(72.74, averageGrades.get(1)[0], FLOATING_NUMBER_VARIANCE);
+		assertEquals(75.04, averageGrades.get(6)[0], FLOATING_NUMBER_VARIANCE);
 		
-		assertEquals(83.32, averageGrades.get(7)[0], FLOATING_NUMBER_VARIANCE);
+		//week 7 should not exist
+		assertNull(averageGrades.get(7));
+	}
+	
+	/**
+	 * Tests methods:
+	 * 
+	 * @see com.revature.caliber.services.ReportingService#getTraineeOverallLineChart(int, int)
+	 */
+	@Test
+	public void getTraineeOverallLineChart(){
+		/*
+		 * Method description:
+		 * input: batch, and trainee
+		 * output: map of week and double array for average with set size of 2
+		 * [0: trainee, 1: batch]
+		 */
+		
+		Map<Integer, Double[]> overallGrades =
+				reportingService.getTraineeOverallLineChart(TEST_BATCH_ID, TEST_TRAINEE_ID);
+		
+		//batch had 7 weeks total
+		assertEquals(7, overallGrades.size());
+		
+		
+		//scores should only have 2 values
+		Double[] weekAverage = overallGrades.get(7);
+		assertEquals(2, weekAverage.length);
 		
 		//week 8 should not exist
-		assertNull(averageGrades.get(8));
+		assertNull(overallGrades.get(8));
+		
 	}
 }
