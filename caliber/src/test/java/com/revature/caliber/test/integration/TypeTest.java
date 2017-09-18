0225a1fcade3e8079bb8a91f92eacceb0b2686b3
TinfoilxD <TinfoilxD@gmail.com>
2017-09-18 14:21:52 -0400

    begun TypeTests

---
+++ caliber/src/test/java/com/revature/caliber/test/integration/TypeTest.java
@@ -0,0 +1,24 @@
+package com.revature.caliber.test.integration;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+import com.revature.caliber.CaliberTest;
+
+public class TypeTest extends CaliberTest {
+	private static Logger log = Logger.getLogger(TypeTest.class);
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////
+	// TYPE API
+	//////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	/**
+	 * Tests methods:
+	 * @see com.revature.caliber.controllers.TypeController#allSkillTypes()
+	 */
+	@Test
+	public void allSkillTypes(){
+		
+	}
+	
+}
