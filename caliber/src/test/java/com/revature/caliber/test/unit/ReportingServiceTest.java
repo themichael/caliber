c31f465ab83d7a55600ebf9e97829103f2c69137
Haisam <elkewidyhaisam@gmail.com>
2017-09-18 16:58:14 -0400

    update tests

--- caliber/src/test/java/com/revature/caliber/test/unit/ReportingServiceTest.java
+++ caliber/src/test/java/com/revature/caliber/test/unit/ReportingServiceTest.java
@@ -1,5 +1,6 @@
 package com.revature.caliber.test.unit;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 
@@ -34,20 +35,43 @@ public class ReportingServiceTest extends CaliberTest{
 	@Test
 	public void getBatchWeekPieChartTest() {
 		
-		Integer batchId = 0;
-		Integer weekNumber = 0;
+		log.info("\n \n \n \n <getBatchWeekPieChartTest> Acquired batch information. BatchId: " + 2201 + " weekNumber: " + 7);
 		
-		Map<QCStatus, Integer> pieChart = reportingService.getBatchWeekPieChart(batchId, weekNumber);
-		assertNotNull(pieChart);
+		Map<QCStatus, Integer> pieChart = reportingService.getBatchWeekPieChart(2201, 7);
+		
+		for (QCStatus key : pieChart.keySet()) {
+			
+			log.info("key: "+ key+ " " + pieChart.get(key));
+			
+		}
+		
+		assertEquals( (Integer) 0, (Integer) pieChart.get(QCStatus.Superstar));
+		assertEquals( (Integer) 9, (Integer) pieChart.get(QCStatus.Good));
+		assertEquals( (Integer) 0, (Integer) pieChart.get(QCStatus.Average));
+		assertEquals( (Integer) 7, (Integer) pieChart.get(QCStatus.Poor));
+		
 		
 	}
 	
 	@Test
 	public void pieChartCurrentWeekQCStatusTest() {
 		
-		Integer batchId = batchDao.findAll().get(1).getBatchId();
+		Integer batchId = 2201;
+		
+		log.info("\n \n \n <pieChartCurrentWeekQCStatusTest> Acquired batch information. BatchId: " + batchId);
 		
 		Map<QCStatus, Integer> pieChart = reportingService.pieChartCurrentWeekQCStatus(batchId);
+		
+		for (QCStatus key : pieChart.keySet()) {
+			
+			log.info("key: "+ key+ " " + pieChart.get(key));
+			
+		}
+		
+		assertEquals( (Integer) 0, (Integer) pieChart.get(QCStatus.Superstar));
+		assertEquals( (Integer) 9, (Integer) pieChart.get(QCStatus.Good));
+		assertEquals( (Integer) 0, (Integer) pieChart.get(QCStatus.Average));
+		assertEquals( (Integer) 7, (Integer) pieChart.get(QCStatus.Poor));
 		
 		assertNotNull(pieChart);
 		
@@ -55,9 +79,17 @@ public class ReportingServiceTest extends CaliberTest{
 	
 	
 	@Test
-	public void getAllBatchesCurrentWeekQCStackedBarChart() {
+	public void getAllBatchesCurrentWeekQCStackedBarChartTest() {
 		
+		
+		log.info("\n \n \n \n \n <getAllBatchesCurrentWeekQCStackedBarChartTest> Acquire dem batches.");
 		List<Object> object = reportingService.getAllBatchesCurrentWeekQCStackedBarChart();
+		
+		for (int i = 0; i < object.size(); i++) {
+			
+			log.info("Batch number " + i + ": " + object.get(i));
+			
+		}
 		
 		assertNotNull(object);
 		
@@ -67,38 +99,61 @@ public class ReportingServiceTest extends CaliberTest{
 	public void getBatchWeekQcOverallBarChart() {
 		
 		Batch batch = batchDao.findAll().get(1);
-		Integer batchId = batch.getBatchId();
-		Integer week = batch.getWeeks();
+		int batchId = batch.getBatchId();
+		int weekNumber = 5;
 		
-		Note note = reportingService.getBatchWeekQcOverallBarChart(batchId, week);
+		log.info("\n \n \n \n \n <getBatchWeekQcOverallBarChart> BatchId: " + batchId + " Week: " + 5);
 		
-		assertNotNull(note);
+		Note note = reportingService.getBatchWeekQcOverallBarChart(batchId, weekNumber);
 		
-	}
-	
-	@Test
-	public void getBatchWeekAvgBarChart() {
+		log.info("<getBatchWeekQcOverallBarChart> Note: " + note);
 		
-		Batch batch = batchDao.findAll().get(1);
-		Integer batchId = batch.getBatchId();
-		Integer week = batch.getWeeks();
-		
-		Map <String, Double[]> weekAvgBarChart = reportingService.getBatchWeekAvgBarChart(batchId, week);
-		
-		assertNotNull(weekAvgBarChart);
+		assertEquals(5, note.getWeek());
+		assertEquals(6438, note.getNoteId());
 		
 	}
 	
 	@Test
+	public void getBatchWeekAvgBarChartTest() {
+		
+		int batchId = 2201;
+		int weekNumber = 5;
+		
+		Map <String, Double[]> weekAvgBarChart = reportingService.getBatchWeekAvgBarChart(batchId, weekNumber);
+		
+		log.info("\n \n \n \n \n <getBatchWeekAvgBarChartTest> Acquiring batch number: " + 2201);
+	
+		for (String key : weekAvgBarChart.keySet()) {
+			
+			log.info("key: " + key + " " + weekAvgBarChart.get(key)[0]);
+			
+		}
+		
+		
+		assertEquals((Double) 88.75, (Double) weekAvgBarChart.get("Project")[0]);
+		assertEquals((Double) 76.109375, (Double) weekAvgBarChart.get("Exam")[0]);
+		assertEquals((Double) 74.375, (Double) weekAvgBarChart.get("Verbal")[0]);
+		
+	}
+	
+	@Test
+	@Ignore
 	public void getBatchWeekSortedBarChartTest() {
 		
-		Batch batch = batchDao.findAll().get(1);
-		Integer batchId = batch.getBatchId();
-		Integer week = batch.getWeeks();
+		int batchId = 2201;
+		int weekNumber = 5;
 		
-		Map<String, Double> sortedBarChart = reportingService.getBatchWeekSortedBarChart(batchId, week);
+		Map<String, Double> sortedBarChart = reportingService.getBatchWeekSortedBarChart(batchId, weekNumber);
 		
-		assertNotNull(sortedBarChart);
+		log.info("\n \n \n \n \n <getBatchWeekSortedBarChartTest> Acquiring batch number: " + batchId);
+		
+		for (String key : sortedBarChart.keySet()) {
+			
+			log.info("key: " + key + " " + sortedBarChart.get(key));
+			
+		}
+		
+		assertEquals((Double) 97.0, (Double) sortedBarChart.get("Khawaja, Ateeb"));
 	
 	}
 		
