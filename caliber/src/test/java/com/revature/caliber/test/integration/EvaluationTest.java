9dc424b7901316e66985898d7553fa11cbe92648
TinfoilxD <TinfoilxD@gmail.com>
2017-09-18 10:04:03 -0400

    tested Evaluationservice#findGradesByWeek()

--- caliber/src/test/java/com/revature/caliber/test/integration/EvaluationTest.java
+++ caliber/src/test/java/com/revature/caliber/test/integration/EvaluationTest.java
@@ -1,16 +1,36 @@
 package com.revature.caliber.test.integration;
 
+import static org.junit.Assert.*;
+
 import java.util.List;
 import java.util.Map;
 
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.revature.caliber.CaliberTest;
+import com.revature.caliber.beans.Batch;
 import com.revature.caliber.beans.Grade;
 import com.revature.caliber.beans.Note;
+import com.revature.caliber.data.BatchDAO;
+import com.revature.caliber.services.EvaluationService;
 
 public class EvaluationTest extends CaliberTest {
 
 
 	private static final String NOT_YET_IMPLEMENTED="Not yet implemented";
+	private static final int TEST_BATCH_ID = 2150;
+	private static final int TEST_ASSESSMENT_WEEK = 7;
+
+
+	
+	@Autowired
+	EvaluationService evaluationService;
+	@Autowired
+	BatchDAO batchDAO;
+	
+	
+	
 	//////////////////////////////////////////////////////////////////////////////////////////////////
 	// EVALUATION API
 	//////////////////////////////////////////////////////////////////////////////////////////////////
@@ -202,6 +222,40 @@ public class EvaluationTest extends CaliberTest {
 	 */
 	public List<Note> findAllTraineeNotes() {
 		throw new UnsupportedOperationException(NOT_YET_IMPLEMENTED);
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////////
+	// EVALUATION SERVICE
+	//////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	/**
+	 * Test methods:
+	 * 
+	 * @see com.revature.caliber.services.EvaluationService#findGradesByWeek(Integer, Integer)
+	 * 
+	 */
+	@Test
+	public void findGradesByWeek(){
+		//get test batch
+		Batch batch = batchDAO.findOne(TEST_BATCH_ID);
+		
+		//get grades
+		Map<Integer,List<Grade>> grades = 
+				evaluationService.findGradesByWeek(batch.getBatchId(), TEST_ASSESSMENT_WEEK);
+		
+		//assert size of result as expected in test data
+		assertEquals(13, grades.size());
+		
+		//iterate through map entries and assert it is of test week
+		for (Map.Entry<Integer, List<Grade>> entry : grades.entrySet())
+		{
+		    List<Grade> weekGrades = entry.getValue();
+		    for(Grade grade: weekGrades){
+		    	assertEquals(TEST_ASSESSMENT_WEEK, grade.getAssessment().getWeek());
+		    }
+		}
+		
+		
 	}
 
 }
