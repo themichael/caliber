473f54ccd8e19d2f635114254f4848438881fb4d
@ForSerious <forxxserious@gmail.com>
2017-09-19 16:52:29 -0400

    I think I made some headway.

--- caliber/src/test/java/com/revature/caliber/test/api/TrainingAPITest.java
+++ caliber/src/test/java/com/revature/caliber/test/api/TrainingAPITest.java
@@ -2,11 +2,18 @@ package com.revature.caliber.test.api;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import java.time.Instant;
+import java.util.Date;
 
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.caliber.beans.Batch;
 import com.revature.caliber.beans.Trainer;
 import com.revature.caliber.beans.TrainerRole;
 
@@ -20,14 +27,15 @@ import io.restassured.http.ContentType;
  * @author Patrick Walsh
  *
  */
-public class TrainingAPITest extends AbstractAPITest{
+public class TrainingAPITest extends AbstractAPITest {
 
 	private static final Logger log = Logger.getLogger(TrainingAPITest.class);
-	
+
 	/*
 	 * Trainer API endpoints
 	 */
 	private String findByEmail = "training/trainer/byemail/patrick.walsh@revature.com/";
+	private String findAllDroppedTrainees = "all/trainee/dropped";
 
 	@Test
 	public void findByEmail() throws Exception {
@@ -35,9 +43,23 @@ public class TrainingAPITest extends AbstractAPITest{
 				TrainerRole.ROLE_VP);
 		expected.setTrainerId(1);
 		log.info("API Testing findTrainerByEmail at baseUrl  " + baseUrl);
-		given().header("Authorization", accessToken).contentType(ContentType.JSON).when()
-				.get(baseUrl + findByEmail).then().assertThat()
-				.statusCode(200).body(matchesJsonSchema(new ObjectMapper().writeValueAsString(expected)));
+		given().header("Authorization", accessToken).contentType(ContentType.JSON).when().get(baseUrl + findByEmail)
+				.then().assertThat().statusCode(200)
+				.body(matchesJsonSchema(new ObjectMapper().writeValueAsString(expected)));
+	}
+
+	/**
+	 * findAllDroppedByBatch(@RequestParam(required = true) Integer batch)
+	 * @throws Exception 
+	 */
+	@Test
+	public void findAllDroppedByBatchTest() throws Exception{
+		
+		log.info("API Testing findAllDroppedByBatchTest at baseUrl  " + baseUrl);
+		String actual = given().header("Authorization", accessToken).contentType(ContentType.JSON).queryParam("batch", "2050").when()
+				.get(baseUrl + findAllDroppedTrainees).then().assertThat().statusCode(200).extract().jsonPath().prettyPrint();
+		System.out.println(actual);
+		assertEquals("2050", actual);
 	}
 
 }
