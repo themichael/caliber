73b5cae02ebc21f559afd18117edf89e42e298a6
Evan Molinelli <emolinelli85@gmail.com>
2017-09-19 09:43:12 -0400

    download pdf tests still in progress

--- caliber/src/test/java/com/revature/caliber/test/uat/ImportBatchFeature.java
+++ caliber/src/test/java/com/revature/caliber/test/uat/ImportBatchFeature.java
@@ -13,16 +13,15 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class ImportBatchFeature {
+public class ImportBatchFeature extends DriverSetup{
 	
-	private HtmlUnitDriver driver;
 	private ManageBatchPage managePage;
 	
 	@Before // each scenario
 	public void setup(){
 		driver = new HtmlUnitDriver(BrowserVersion.CHROME);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		managePage = new ManageBatchPage(driver);
+		managePage = new ManageBatchPage();
 	}
 	@After // each scenario
 	public void teardown(){
