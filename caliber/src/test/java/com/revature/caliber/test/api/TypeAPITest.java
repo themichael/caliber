819e6093115d9eaccb49d7fbebd97c30df1f761f
TinfoilxD <TinfoilxD@gmail.com>
2017-09-19 14:22:41 -0400

    tested typecontroller

--- caliber/src/test/java/com/revature/caliber/test/api/TypeAPITest.java
+++ caliber/src/test/java/com/revature/caliber/test/api/TypeAPITest.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import java.util.List;
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.not;
 
 import org.apache.log4j.Logger;
 import org.junit.Test;
@@ -37,19 +39,143 @@ public class TypeAPITest extends AbstractAPITest {
 	 */
 	@Test
 	public void allSkillTypes() throws Exception {
-		
-		List<String> expected = new ArrayList<>(Arrays.asList("J2EE", ".NET", "SDET", "BPM", "Other"));
-		
-		
-		String expectedJSON = new ObjectMapper().writeValueAsString(expected);
 
-		log.info("API Testing allSkippTypes at baseUrl  " + baseUrl);
-		
+		log.info("API Testing allSkillTypes at baseUrl  " + baseUrl);
+
 		given().header("Authorization", accessToken).contentType(ContentType.JSON)
+		//get request for all skills
 		.when().get(baseUrl + allSkillTypes)
+		//assertions
 		.then().assertThat().statusCode(200)
-		.body(matchesJsonSchema(expectedJSON));
+			.body("$", hasItems("J2EE", ".NET", "SDET", "BPM", "Other"))
+			.body("$", not(hasItems("Cukes", "Parfait")));
 	
 	}
+	
+	/**
+	 * Tests methods:
+	 * 
+	 * @see com.revature.caliber.controllers.TypeController#allTrainingTypes()
+	 */
+	@Test
+	public void allTrainingTypes(){
+		
+		log.info("API Testing allTrainingTypes at baseUrl  " + baseUrl);
 
+		given().header("Authorization", accessToken).contentType(ContentType.JSON)
+		//get request for all training types
+		.when().get(baseUrl + allTrainingTypes)
+		//assertions
+		.then().assertThat().statusCode(200)
+			.body("$", hasItems("Revature", "Corporate", "University", "Other"))
+			.body("$", not(hasItems("Gherkins", "Parfait")));
+		
+		
+	}
+	
+	/**
+	 * Tests methods:
+	 * 
+	 * @see com.revature.caliber.controllers.TypeController#allTrainingStatusTypes()
+	 */
+	@Test
+	public void allTrainingStatusTypes(){
+		
+		log.info("API Testing allTrainingStatusTypes at baseUrl  " + baseUrl);
+
+		given().header("Authorization", accessToken).contentType(ContentType.JSON)
+		//get request for all training status
+		.when().get(baseUrl + allTrainingStatus)
+		//assertions
+		.then().assertThat().statusCode(200)
+			.body("$", hasItems("Signed", "Selected", "Training",
+					"Marketing", "Confirmed", "Employed", "Dropped"))
+			.body("$", not(hasItems("Gherkins", "Tofu Steak")));
+		
+		
+	}
+	
+	/**
+	 * Tests methods:
+	 * 
+	 * @see com.revature.caliber.controllers.TypeController#allNoteTypes()
+	 */
+	@Test
+	public void allNoteTypes(){
+		
+		log.info("API Testing allNoteTypes at baseUrl  " + baseUrl);
+
+		given().header("Authorization", accessToken).contentType(ContentType.JSON)
+		//get request for all note types
+		.when().get(baseUrl + allNoteTypes)
+		//assertions
+		.then().assertThat().statusCode(200)
+			.body("$", hasItems("TRAINEE", "BATCH", "QC_TRAINEE","QC_BATCH"))
+			.body("$", not(hasItems("Pudding a la mode", "Tofu Steak")));
+		
+	}
+	
+	/**
+	 * Tests methods:
+	 * 
+	 * @see com.revature.caliber.controllers.TypeController#allQCStatusTypes()
+	 */
+	@Test
+	public void allQCStatusTypes(){
+		
+		log.info("API Testing allQCStatusTypes at baseUrl  " + baseUrl);
+
+		given().header("Authorization", accessToken).contentType(ContentType.JSON)
+		//get request for all QCStatusTypes
+		.when().get(baseUrl + allQCStatusTypes)
+		//assertions
+		.then().assertThat().statusCode(200)
+			.body("$", hasItems("Superstar", "Good", "Average","Poor"))
+			.body("$", not(hasItems("Pudding a la mode", "Crepe")));
+		
+		
+	}
+	
+	/**
+	 * Tests methods:
+	 * 
+	 * @see com.revature.caliber.controllers.TypeController#allAssessmentTypes()
+	 */
+	@Test
+	public void allAssessmentTypes(){
+		
+		log.info("API Testing allAssessmentTypes at baseUrl  " + baseUrl);
+
+		given().header("Authorization", accessToken).contentType(ContentType.JSON)
+		//get request for all assessment types
+		.when().get(baseUrl + allAssessmentTypes)
+		//assertions
+		.then().assertThat().statusCode(200)
+			.body("$", hasItems("Exam", "Verbal", "Project","Other"))
+			.body("$", not(hasItems("Sundae", "Mousse")));
+		
+		
+	}
+	
+	/**
+	 * Tests methods:
+	 * 
+	 * @see com.revature.caliber.controllers.TypeController#allTrainerRoles()
+	 */
+	@Test
+	public void allTrainerRoles(){
+		
+		
+		log.info("API Testing allTrainingRoles at baseUrl  " + baseUrl);
+
+		given().header("Authorization", accessToken).contentType(ContentType.JSON)
+		//get request for all trainer roles
+		.when().get(baseUrl + allTrainerRoles)
+		//assertions
+		.then().assertThat().statusCode(200)
+			.body("$", hasItems("ROLE_VP", "ROLE_QC", "ROLE_TRAINER","ROLE_STAGING", "ROLE_INACTIVE"))
+			.body("$", not(hasItems("Che", "Mousse")));
+		
+		
+	}
 }
