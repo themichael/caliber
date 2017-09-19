8747777594727ce03460b6a790fbb40defb89f5e
Daniel <felix0654@gmail.com>
2017-09-19 14:36:43 -0400

    findallqcbatchnotes, findallqctraineenotes, findallqctraineeoverallnotes change number:2

--- caliber/src/test/java/com/revature/caliber/test/unit/NoteDAOTest.java
+++ caliber/src/test/java/com/revature/caliber/test/unit/NoteDAOTest.java
@@ -23,7 +23,7 @@ public class NoteDAOTest extends CaliberTest {
 	private static final Logger log = Logger.getLogger(NoteDAOTest.class);
 	private static final int TEST_BATCH_ID = 2201;
 	private static final int TEST_WEEK=7;
-	private static final int TEST_TRAINER_ID=5524;
+	private static final int TEST_TRAINEE_ID=5524;
 	
 	@Autowired
 	private NoteDAO noteDao;
@@ -85,7 +85,12 @@ public class NoteDAOTest extends CaliberTest {
 	public void findAllQCBatchNotes(){
 		log.trace("GETTING ALL QC BATCH NOTES");
 		List<Note> notes = noteDAO.findAllQCBatchNotes(TEST_BATCH_ID);
+		
+		int[] expected = {6369,6390,6391,6420,6438,6457,6470};
 		assertTrue(notes.size()>0);
+		for(int j = 0; j<expected.length; j++){
+		assertEquals(expected[j], notes.get(j).getNoteId());
+		}
 	}
 	
 	@Test	
@@ -93,12 +98,22 @@ public class NoteDAOTest extends CaliberTest {
 		log.trace("GETTING ALL QC TRAINEE NOTES");
 		List<Note> notes = noteDAO.findAllQCTraineeNotes(TEST_BATCH_ID, TEST_WEEK);
 		assertTrue(notes.size()>0);
+		int[] expected = {6459,6460,6461,6462,6463,6464,6465,6466,6467,6468,6469,6471,6472,6473,6474,6475};
+		for(int j = 0; j<expected.length; j++){
+			assertEquals(expected[j], notes.get(j).getNoteId());
+		}
 	}
+	
 	
 	@Test	
 	public void findAllQCTraineeOverallNotes(){
 		log.trace("GETTING ALL QC TRAINEE OVERALL NOTES");
-		List<Note> notes = noteDAO.findAllQCTraineeOverallNotes(TEST_TRAINER_ID);
+		List<Note> notes = noteDAO.findAllQCTraineeOverallNotes(TEST_TRAINEE_ID);
 		assertTrue(notes.size()>0);
+		int[] expected = {6355,6381,6394,6439,6423,6453,6463};
+		for(int j = 0; j<expected.length; j++){
+			assertEquals(expected[j], notes.get(j).getNoteId());
+		}
 	}
+	
 }
