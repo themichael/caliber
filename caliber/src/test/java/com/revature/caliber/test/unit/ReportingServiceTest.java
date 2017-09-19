48b9a82c391e75f1ac400c7326a06cbe9be08242
Patrick <pburns96@live.com>
2017-09-19 11:25:35 -0400

    Formatted ReportingServiceTest.java

--- caliber/src/test/java/com/revature/caliber/test/unit/ReportingServiceTest.java
+++ caliber/src/test/java/com/revature/caliber/test/unit/ReportingServiceTest.java
@@ -26,160 +26,155 @@ import com.revature.caliber.services.ReportingService;
 public class ReportingServiceTest extends CaliberTest {
 
 	private static final Logger log = Logger.getLogger(ReportingServiceTest.class);
-	
-	
+
 	@Autowired
 	ReportingService reportingService;
-	
-	//BatchDAO is only autowired here to get one batch from the database and use it's id number.
+
+	// BatchDAO is only autowired here to get one batch from the database and
+	// use it's id number.
 	@Autowired
 	BatchDAO batchDao;
-	
-	
+
+	public void setReportingService(ReportingService reportingService) {
+		this.reportingService = reportingService;
+	}
+
 	@Test
 	public void getBatchWeekPieChartTest() {
-		
+
 		Integer batchId = 0;
 		Integer weekNumber = 0;
-		
+
 		Map<QCStatus, Integer> pieChart = reportingService.getBatchWeekPieChart(batchId, weekNumber);
 		assertNotNull(pieChart);
-		
+
 	}
-	
+
 	@Test
 	public void pieChartCurrentWeekQCStatusTest() {
-		
+
 		Integer batchId = batchDao.findAll().get(1).getBatchId();
-		
+
 		Map<QCStatus, Integer> pieChart = reportingService.pieChartCurrentWeekQCStatus(batchId);
-		
+
 		assertNotNull(pieChart);
-		
+
 	}
-	
-	
+
 	@Test
 	public void getAllBatchesCurrentWeekQCStackedBarChart() {
-		
+
 		List<Object> object = reportingService.getAllBatchesCurrentWeekQCStackedBarChart();
-		
+
 		assertNotNull(object);
-		
+
 	}
-	
+
 	@Test
 	public void getBatchWeekQcOverallBarChart() {
-		
+
 		Batch batch = batchDao.findAll().get(1);
 		Integer batchId = batch.getBatchId();
 		Integer week = batch.getWeeks();
-		
+
 		Note note = reportingService.getBatchWeekQcOverallBarChart(batchId, week);
-		
+
 		assertNotNull(note);
-		
+
 	}
-	
+
 	@Test
 	public void getBatchWeekAvgBarChart() {
-		
+
 		Batch batch = batchDao.findAll().get(1);
 		Integer batchId = batch.getBatchId();
 		Integer week = batch.getWeeks();
-		
-		Map <String, Double[]> weekAvgBarChart = reportingService.getBatchWeekAvgBarChart(batchId, week);
-		
+
+		Map<String, Double[]> weekAvgBarChart = reportingService.getBatchWeekAvgBarChart(batchId, week);
+
 		assertNotNull(weekAvgBarChart);
-		
+
 	}
-	
+
 	@Test
 	public void getBatchWeekSortedBarChartTest() {
-		
+
 		Batch batch = batchDao.findAll().get(1);
 		Integer batchId = batch.getBatchId();
 		Integer week = batch.getWeeks();
-		
+
 		Map<String, Double> sortedBarChart = reportingService.getBatchWeekSortedBarChart(batchId, week);
-		
+
 		assertNotNull(sortedBarChart);
-	
+
 	}
-		
+
 	@Test
-	public void  getBatchOverallBarChart(){
-		//Positive Testing
+	public void getBatchOverallBarChart() {
+		// Positive Testing
 		Map<String, Double> results = reportingService.getBatchOverallBarChart(2200);
-		assertTrue("Test size of result set", results.size()== 15);
+		assertTrue("Test size of result set", results.size() == 15);
 		assertTrue("Contains expected trainee", results.containsKey("Chen, Andrew"));
 		assertTrue("Test accurate average calculation", results.get("Chen, Andrew").doubleValue() == 84.14575d);
-		
-		//Negative Testing
-		//Grab non-existent batch
-		try{
+
+		// Negative Testing
+		// Grab non-existent batch
+		try {
 			results = reportingService.getBatchOverallBarChart(-1111);
 			fail();
-		}catch(NullPointerException e){
+		} catch (NullPointerException e) {
 			log.debug(e);
 		}
 	}
+
 	@Test
-	public void getBatchWeekTraineeBarChart(){
-		//Positive testing
+	public void getBatchWeekTraineeBarChart() {
+		// Positive testing
 		Map<String, Double[]> results = reportingService.getBatchWeekTraineeBarChart(2100, 5455, 1);
 		assertNotNull("Results exist", results);
 		assertTrue("Test size of result set", results.size() == 1);
 		assertTrue("Result contains exam", results.containsKey("Exam"));
-		assertTrue("Exam contains expected values", results.get("Exam")[0] == 93.0 
-				& results.get("Exam")[1] == 85.625
-				& results.get("Exam")[2] == 100);
+		assertTrue("Exam contains expected values",
+				results.get("Exam")[0] == 93.0 & results.get("Exam")[1] == 85.625 & results.get("Exam")[2] == 100);
 
-		//Invalid TraineeID
-		try{
-		results = reportingService.getBatchWeekTraineeBarChart(2100, -123421, 1);
-		fail();
-		}catch(NoSuchElementException e){
+		// Invalid TraineeID
+		try {
+			results = reportingService.getBatchWeekTraineeBarChart(2100, -123421, 1);
+			fail();
+		} catch (NoSuchElementException e) {
 			log.info(e);
 		}
 
 		results = reportingService.getBatchWeekTraineeBarChart(2100, 5455, -1000);
 		assertTrue("Check invalid week", results.size() == 0);
 	}
+
 	@Test
 	public void getBatchOverallTraineeBarChart() {
-		//Training
+		// Training
 		Map<String, Double[]> results = reportingService.getBatchOverallTraineeBarChart(2201, 5531);
-	    assertNotNull("Results exist", results);
+		assertNotNull("Results exist", results);
 		assertTrue("Test size of result set ", results.size() == 4);
-		
+
 		assertTrue("Test data exists", results.containsKey("Exam"));
 		Double[] myVals = results.get("Verbal");
 		assertTrue("Test values of Verbal exam", myVals[0] == 69.2 & myVals[1] == 82.0);
-		
-		//Invalid TraineeID
-		try{
+
+		// Invalid TraineeID
+		try {
 			results = reportingService.getBatchOverallTraineeBarChart(2100, -123421);
 			fail();
-		}catch(NoSuchElementException e){
-				log.info(e);
+		} catch (NoSuchElementException e) {
+			log.info(e);
 		}
 
-		
-		
-		
-
-	}
-
-	public void setReportingService(ReportingService reportingService) {
-		this.reportingService = reportingService;
 	}
 
 	/**
-	 * Utility method for testing
-	 * Creates and returns a batch with 3 trainees with QC Notes and statuses for 7 weeks
+	 * Utility method for testing Creates and returns a batch with 3 trainees
+	 * with QC Notes and statuses for 7 weeks
 	 * 
-	 * @return Batch 
+	 * @return Batch
 	 */
 	private Batch createTestBatchWithQCNotes() {
 		Set<Trainee> trainees = new HashSet<>();
@@ -221,20 +216,21 @@ public class ReportingServiceTest extends CaliberTest {
 
 	/**
 	 * Tests methods:
+	 * 
 	 * @see com.revature.caliber.services.ReportingService#utilSeparateQCTraineeNotesByWeekTest()
 	 */
 	@Test
 	public void utilSeparateQCTraineeNotesByWeekTest() {
 
 		log.info("Testing the ReportingService.utilSeperateQCTraineeNotesByWeek");
-		
+
 		int[] statusPoorCountPerWeek = { 0, 0, 0, 3, 0, 0, 0 };
 		int[] statusAverageCountPerWeek = { 3, 0, 0, 0, 3, 0, 0 };
 		int[] statusGoodCountPerWeek = { 0, 3, 0, 0, 0, 3, 0 };
 		int[] statusSuperstarCountPerWeek = { 0, 0, 3, 0, 0, 0, 3 };
-		
+
 		Batch batch = createTestBatchWithQCNotes();
-		
+
 		Map<Integer, Map<QCStatus, Integer>> results = reportingService.utilSeparateQCTraineeNotesByWeek(batch);
 		for (int i = 1; i <= batch.getWeeks(); i++) {
 			Map<QCStatus, Integer> weekStatusCount = results.get(i);
@@ -268,5 +264,3 @@ public class ReportingServiceTest extends CaliberTest {
 	}
 
 }
-		
-
