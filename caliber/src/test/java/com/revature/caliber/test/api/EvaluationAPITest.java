7cb1480ca4c0d57bdda5525beae38bc1b2c9f05a
Patrick <patrick1jack@gmail.com>
2017-09-20 11:11:48 -0400

    started api tests

--- caliber/src/test/java/com/revature/caliber/test/api/EvaluationAPITest.java
+++ caliber/src/test/java/com/revature/caliber/test/api/EvaluationAPITest.java
@@ -3,11 +3,19 @@ package com.revature.caliber.test.api;
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.caliber.beans.Batch;
 import com.revature.caliber.beans.Grade;
+import com.revature.caliber.beans.Note;
+import com.revature.caliber.beans.NoteType;
+import com.revature.caliber.beans.QCStatus;
+import com.revature.caliber.beans.Trainee;
 import com.revature.caliber.beans.Trainer;
 import com.revature.caliber.beans.TrainerRole;
 import com.revature.caliber.data.GradeDAO;
@@ -19,12 +27,12 @@ public class EvaluationAPITest extends AbstractAPITest{
 	private static final Logger log = Logger.getLogger(EvaluationAPITest.class);
 	
 	@Test
-	public void createGrade(Grade grade){
+	public void createGrade(){
 		GradeDAO gradeDAO = new GradeDAO();
 		log.info("");
 	}
 	@Test
-	public void updateGrade(Grade grade){
+	public void updateGrade(){
 		
 	}
 	@Test
@@ -32,7 +40,7 @@ public class EvaluationAPITest extends AbstractAPITest{
 		
 	}
 	@Test
-	public void findByAssessment(Long assessmentId){
+	public void findByAssessment(){
 		
 	}
 	
@@ -83,13 +91,20 @@ public class EvaluationAPITest extends AbstractAPITest{
 	
 	@Test
 	public void createNote() throws Exception {
-		Trainer expected = new Trainer("Patrick Walsh", "Lead Trainer", "patrick.walsh@revature.com",
-				TrainerRole.ROLE_VP);
-		expected.setTrainerId(1);
 		
-		log.info("API Testing findTrainerByEmail at baseUrl  " + baseUrl);
-		given().header("Authorization", accessToken).contentType(ContentType.JSON).when()
-				.get(baseUrl + findByEmail).then().assertThat()
-				.statusCode(200).body(matchesJsonSchema(new ObjectMapper().writeValueAsString(expected)));
+		String createNote = "trainer/grade/create";
+		Batch batch = null;
+		
+		Note expected = new Note();
+		expected.setContent("This is a test note");
+		expected.setWeek((short) 2);
+		expected.setBatch(batch);
+		expected.setType(NoteType.TRAINEE);
+		expected.setQcStatus(QCStatus.Poor);
+		
+		log.info("API Testing createNote at " + baseUrl + createNote);
+		given().spec(requestSpec).header(authHeader, accessToken)
+				.contentType(ContentType.JSON).body(new ObjectMapper().writeValueAsString(expected)).when()
+				.post(baseUrl + createNote).then().assertThat().statusCode(201);
 	}
 }
