89e400ea403c240ab3faa1360778e4b35feab2fd
@ForSerious <forxxserious@gmail.com>
2017-09-20 11:03:57 -0400

    Made my first API test.

--- caliber/src/test/java/com/revature/caliber/test/api/TrainingAPITest.java
+++ caliber/src/test/java/com/revature/caliber/test/api/TrainingAPITest.java
@@ -2,22 +2,20 @@ package com.revature.caliber.test.api;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
-import java.time.Instant;
-import java.util.Date;
 
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.revature.caliber.beans.Batch;
+import com.revature.caliber.beans.Trainee;
 import com.revature.caliber.beans.Trainer;
 import com.revature.caliber.beans.TrainerRole;
+import com.revature.caliber.beans.TrainingStatus;
 
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 
 /**
  * API testing at the RESTful service message layer using REST Assured. All API
@@ -57,10 +55,16 @@ public class TrainingAPITest extends AbstractAPITest {
 	public void findAllDroppedByBatchTest() throws Exception{
 		
 		log.info("API Testing findAllDroppedByBatchTest at baseUrl  " + baseUrl);
-		String actual = given().header("Authorization", accessToken).contentType(ContentType.JSON).queryParam("batch", "2050").when()
-				.get(baseUrl + findAllDroppedTrainees).then().assertThat().statusCode(200).extract().jsonPath().prettyPrint();
-		System.out.println(actual);
-		assertEquals("2050", actual);
+		Response actual = given().spec(requestSpec).header("Authorization", accessToken).contentType(ContentType.JSON).queryParam("batch", "2050").when()
+				.get(baseUrl + findAllDroppedTrainees).then().assertThat().statusCode(200).extract().response();
+		Trainee[] resultSet = actual.as(Trainee[].class);
+		boolean success = true;
+		for(Trainee dude : resultSet){
+			if(dude.getTrainingStatus() != TrainingStatus.Dropped){
+				success = false;
+			}
+		}
+		assertTrue(success);
 	}
 
 	@Test
