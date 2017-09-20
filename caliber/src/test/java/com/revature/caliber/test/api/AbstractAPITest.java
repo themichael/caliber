3254e494ce351dd451b8c051f9e32910688adce5
Patrick <patrick.walsh@revature.com>
2017-09-20 16:31:39 -0400

    local stash

--- caliber/src/test/java/com/revature/caliber/test/api/AbstractAPITest.java
+++ caliber/src/test/java/com/revature/caliber/test/api/AbstractAPITest.java
@@ -2,6 +2,7 @@ package com.revature.caliber.test.api;
 
 import static io.restassured.RestAssured.given;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,9 +15,11 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.log4j.Logger;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.caliber.CaliberTest;
+import com.revature.caliber.security.models.SalesforceToken;
 
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.Response;
@@ -40,23 +43,23 @@ public abstract class AbstractAPITest extends CaliberTest {
 	 * Salesforce access token to be used in Authorization HTTP header
 	 */
 	protected static String accessToken = "Auth ";
-	protected static final String authHeader = "Authorization";
+	protected static final String auth = "Authorization";
 	protected static RequestSpecification requestSpec;
 	
-	protected String baseUrl = System.getenv("CALIBER_SERVER_URL");
-	private String username = System.getenv("CALIBER_API_USERNAME");
-	private String password = System.getenv("CALIBER_API_PASSWORD");
-	private String clientId = System.getenv("SALESFORCE_CLIENT_ID");
-	private String clientSecret = System.getenv("SALESFORCE_CLIENT_SECRET");
-	private String accessTokenUrl = "https://test.salesforce.com/services/oauth2/token";
+	protected static String baseUrl = System.getenv("CALIBER_SERVER_URL");
+	private static String username = System.getenv("CALIBER_API_USERNAME");
+	private static String password = System.getenv("CALIBER_API_PASSWORD");
+	private static String clientId = System.getenv("SALESFORCE_CLIENT_ID");
+	private static String clientSecret = System.getenv("SALESFORCE_CLIENT_SECRET");
+	private static String accessTokenUrl = "https://test.salesforce.com/services/oauth2/token";
 	
 	private static final Logger log = Logger.getLogger(AbstractAPITest.class);
 
 	public AbstractAPITest() {
 		// only login with Salesforce once
-		if (accessToken.equals("Auth ")) {
+		if ("Auth ".equals(accessToken)) {
 			try {
-				//login();
+				login();
 				log.info("Logging into Caliber for API testing");
 				Response response = given().redirects().allowCircular(true).get(baseUrl + "caliber/");
                 String sessionCookie = response.getCookie("JSESSIONID");
@@ -68,7 +71,7 @@ public abstract class AbstractAPITest extends CaliberTest {
 		}
 	}
 
-	private void login() throws Exception {
+	private static void login() throws JsonParseException, JsonMappingException, UnsupportedOperationException, IOException {
 		HttpClient httpClient = HttpClientBuilder.create().build();
 		log.info("logging into URL   " + accessTokenUrl );
 		HttpPost post = new HttpPost(accessTokenUrl);
@@ -81,53 +84,9 @@ public abstract class AbstractAPITest extends CaliberTest {
 		post.setEntity(new UrlEncodedFormEntity(parameters));
 		HttpResponse response = httpClient.execute(post);
 		accessToken += new ObjectMapper().readValue(response.getEntity().getContent(), 
-				JsonNode.class); // test
-				//SalesforceToken.class).getAccessToken(); // actual
+				//JsonNode.class); // test
+				SalesforceToken.class).getAccessToken(); // actual
 		log.info("Accessing Salesforce API using token:  " + accessToken);
 	}
-
-	public String getAccessToken() {
-		return accessToken;
-	}
-
-	public String getBaseUrl() {
-		return baseUrl;
-	}
-
-	public void setBaseUrl(String baseUrl) {
-		this.baseUrl = baseUrl;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getClientId() {
-		return clientId;
-	}
-
-	public void setClientId(String clientId) {
-		this.clientId = clientId;
-	}
-
-	public String getClientSecret() {
-		return clientSecret;
-	}
-
-	public void setClientSecret(String clientSecret) {
-		this.clientSecret = clientSecret;
-	}
-
+	
 }
