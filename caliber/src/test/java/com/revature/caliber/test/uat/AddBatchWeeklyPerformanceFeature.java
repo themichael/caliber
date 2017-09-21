85148ed1883735575638fe65acaa239f208d4a07
Omokorede <koredeallen@gmail.com>
2017-09-21 11:59:40 -0400

    changes

--- caliber/src/test/java/com/revature/caliber/test/uat/AddBatchWeeklyPerformanceFeature.java
+++ caliber/src/test/java/com/revature/caliber/test/uat/AddBatchWeeklyPerformanceFeature.java
@@ -1,5 +1,6 @@
 package com.revature.caliber.test.uat;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -19,6 +20,7 @@ import cucumber.api.java.en.When;
 public class AddBatchWeeklyPerformanceFeature {
 
 	private AssessBatchPage assessBatch;
+	private SettingsTrainerPage trainerPage;
 	
 	@cucumber.api.java.Before
 	public void setup(){
@@ -29,36 +31,36 @@ public class AddBatchWeeklyPerformanceFeature {
 		driver.manage().window().setSize(new Dimension(1200, 1000));
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		assessBatch = new AssessBatchPage(driver);
+		trainerPage = new SettingsTrainerPage(driver);
 	}
 	
 	@Given("^I am in the Assess Batch Page$")
 	public void i_am_in_the_Assess_Batch_Page() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-		assessBatch.gotoPage();
-		assessBatch.verifyAssessBatchPage();
+//		assessBatch.gotoPage();
+//		assessBatch.verifyAssessBatchPage();
+		trainerPage.gotoPage();
+		trainerPage.verifyPage();
 	}
 
 	@Given("^I have chosen the Week tab$")
 	public void i_have_chosen_the_Week_tab() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-		assessBatch.clickWeekTab();
+//		assessBatch.clickWeekTab();
+		trainerPage.createTrainerTab();
+		trainerPage.enterName("korede");
 	}
 	
 	@Given("^I have entered \"([^\"]*)\" as the Overall Feedback$")
-	public void iHaveEnteredAsTheOverallFeedback(String feedback){
-		// Write code here that turns the phrase above into concrete actions
-		//assessBatch.enterFeedback("kjdkjd");
+	public void iHaveEnteredAsTheOverallFeedback(String feedback) throws InterruptedException, IOException{
+		//assessBatch.enterFeedback("kjdkjd");	
 	}
 
 	@When("^I press the Save button$")
 	public void i_press_the_Save_button() throws Throwable {
-	    // Write code here that turns the phrase above into concrete action
-		//assessBatch.saveButton();
+		//assessBatch.saveButton();	
 	}
 
 	@Then("^the Feedback is recorded$")
 	public void theFeedbackIsRecorded() throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
 	
 	}
 
---
+++ caliber/src/test/java/com/revature/caliber/test/uat/SettingsTrainerPage.java
@@ -0,0 +1,176 @@
+package com.revature.caliber.test.uat;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.support.ui.Select;
+
+/**
+ * 
+ * @author KoredeA Page Object Model for the Settings Trainer Page In this Page
+ *         the user is able to delete, update and Create a trainer Use the
+ *         methods below, you are able to do every possible action allowed on
+ *         the Settings Trainer page
+ *
+ */
+public class SettingsTrainerPage {
+
+	private PhantomJSDriver driver;
+
+	public SettingsTrainerPage(WebDriver driver) {
+		this.driver = (PhantomJSDriver) driver;
+	}
+
+	// Sends driver to the specified page
+	public void gotoPage() throws InterruptedException {
+		driver.get("http://localhost:8080/caliber#/vp/trainers");
+		Thread.sleep(115);
+	}
+
+	// Checks to see if the driver is currently on the specified page
+	public void verifyPage() {
+		assertEquals("http://localhost:8080/caliber#/vp/trainers", driver.getCurrentUrl());
+	}
+
+	// Clicks the CreateTrainer button to open create Trainer modal
+	public void createTrainerTab() throws InterruptedException {
+		driver.findElement(
+				By.cssSelector("body > div > ui-view > ui-view > div > div:nth-child(1) > div > div > ul > li > a"))
+				.click();
+		driver.switchTo().activeElement();
+		Thread.sleep(115);
+	}
+
+	// Enters name into the Trainer name input field
+	public void enterName(String name) {
+		driver.findElement(By.cssSelector(
+				"#createTrainerModal > div > div > div.modal-body > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > input"))
+				.sendKeys(name);
+	}
+
+	// Enters email into the Email input field
+	public void enterEmail(String email) {
+		driver.findElement(By.cssSelector(
+				"#createTrainerModal > div > div > div.modal-body > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > input"))
+				.sendKeys(email);
+	}
+
+	// Enters name of Title into the title input field
+	public void selectTitle(String title) {
+		driver.findElement(By.cssSelector(
+				"#createTrainerModal > div > div > div.modal-body > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > input"))
+				.sendKeys(title);
+
+	}
+
+	// Selects the Tier option from the dropdown box
+	public void selectTier(String tier) throws InterruptedException {
+		Select dropdown = new Select(driver.findElement(By.cssSelector(
+				"#createTrainerModal > div > div > div.modal-body > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > select")));
+		Thread.sleep(115);
+		dropdown.selectByVisibleText(tier);
+	}
+
+	// clicks the save button in the Modal of the Create Trainer
+	public void createTrainerModalSaveButton() throws IOException, InterruptedException {
+		driver.findElement(
+				By.cssSelector("#createTrainerModal > div > div > div.modal-body > div.modal-footer > input")).click();
+		Thread.sleep(115);
+	}
+
+	// clicks the close button in the Modal of the Create Trainer
+	public void createTrainerCloseButton() {
+		driver.findElement(
+				By.cssSelector("#createTrainerModal > div > div > div.modal-body > div.modal-footer > button")).click();
+	}
+
+	// clicks the pencil glyphicon which brings up the update Trainer modal
+	public void updateButton() throws InterruptedException {
+		driver.findElement(By.cssSelector(
+				"body > div > ui-view > ui-view > div > div:nth-child(2) > div > div > table > tbody > tr:nth-child(1) > td:nth-child(5) > a"))
+				.click();
+		Thread.sleep(115);
+	}
+
+	// clears the input field for the Trainer name then updates the name
+	public void updateTrainerName(String name) {
+		driver.switchTo().activeElement();
+		driver.findElement(By.cssSelector(
+				"#editTrainerModal > div > div > div.modal-body > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)>input"))
+				.clear();
+		driver.findElement(By.cssSelector(
+				"#editTrainerModal > div > div > div.modal-body > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)>input"))
+				.sendKeys(name);
+		;
+	}
+
+	// clears the input field for the Trainer email then updates the email
+	public void updateEmail(String email) {
+		driver.findElement(By.cssSelector(
+				"#editTrainerModal > div > div > div.modal-body > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > input"))
+				.clear();
+		driver.findElement(By.cssSelector(
+				"#editTrainerModal > div > div > div.modal-body > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > input"))
+				.sendKeys(email);
+
+	}
+
+	// clears the input field for the Trainer title then updates the title
+	public void updateTitle(String title) {
+		driver.findElement(By.cssSelector(
+				"#editTrainerModal > div > div > div.modal-body > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > input"))
+				.clear();
+		driver.findElement(By.cssSelector(
+				"#editTrainerModal > div > div > div.modal-body > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > input"))
+				.sendKeys(title);
+	}
+
+	// Selects a tier from the dropdown
+	public void updateTier(String tier) throws InterruptedException {
+		Select dropdown = new Select(driver.findElement(By.cssSelector(
+				"#editTrainerModal > div > div > div.modal-body > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > select")));
+		Thread.sleep(115);
+		dropdown.selectByVisibleText(tier);
+	}
+
+	// clicks the update button inside the update modal
+	public void modalUpdateButton() throws IOException, InterruptedException {
+		driver.findElement(By.cssSelector("#editTrainerModal > div > div > div.modal-body > div.modal-footer > input"))
+				.click();
+		Thread.sleep(115);
+	}
+
+	// clicks the close button inside the update modal
+	public void updateModalCloseButton() throws InterruptedException, IOException {
+		driver.findElement(By.cssSelector("#editTrainerModal > div > div > div.modal-body > div.modal-footer > button"))
+				.click();
+		Thread.sleep(115);
+	}
+
+	// clicks the x glyphicon which brings up the delete Trainer modal to the
+	// screen
+	public void deleteButton() {
+		driver.findElement(By.cssSelector(
+				"body > div > ui-view > ui-view > div > div:nth-child(2) > div > div > table > tbody > tr:nth-child(1) > td:nth-child(6) > a"))
+				.click();
+	}
+
+	// clicks the close button inside the delete modal
+	public void deleteModalCloseButton() {
+		driver.findElement(By.cssSelector("#deleteTrainerModal > div > div > div.modal-footer > button")).click();
+	}
+
+	// clicks the delete button inside the delete modal
+	public void modalDeleteButton() {
+		driver.findElement(By.cssSelector("#deleteTrainerModal > div > div > div.modal-footer > input")).click();
+	}
+
+}
