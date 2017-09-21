a5459f97add5ec5956a10bca3fe15113e6ef8a7a
Patrick <patrick.walsh@revature.com>
2017-09-21 09:56:57 -0400

    ci

--- caliber/src/test/java/com/revature/caliber/test/api/AbstractAPITest.java
+++ caliber/src/test/java/com/revature/caliber/test/api/AbstractAPITest.java
@@ -61,7 +61,7 @@ public abstract class AbstractAPITest extends CaliberTest {
 			try {
 				login();
 				log.info("Logging into Caliber for API testing");
-				Response response = given().body("salestoken="+accessToken).redirects().allowCircular(true).get(baseUrl + "caliber/");
+				Response response = given().redirects().allowCircular(true).get(baseUrl + "caliber/");
                 String sessionCookie = response.getCookie("JSESSIONID");
                 String roleCookie = response.getCookie("role");
                 requestSpec = new RequestSpecBuilder().addCookie("JSESSIONID", sessionCookie ).addCookie("role", roleCookie).build();
