655d4770d155b3bf291221ebcc6ae70df683ace3
Patrick <patrick1jack@gmail.com>
2017-09-23 10:06:14 -0400

    Revert "testing possible flash attribute fix"
    This reverts commit 997a2409493db2cd43c737db63cb22cc4782b377.

--- caliber/src/main/java/com/revature/caliber/controllers/BootController.java
+++ caliber/src/main/java/com/revature/caliber/controllers/BootController.java
@@ -91,7 +91,7 @@ public class BootController extends AbstractSalesforceSecurityHelper {
 		try {
 			log.debug("About to check for salesforce token");
 			SalesforceToken salesforceToken = getSalesforceToken(salesTokenString);
-			model.asMap().clear();
+			//model.asMap().clear();
 			// Http request to the salesforce module to get the Salesforce user
 			SalesforceUser salesforceUser = getSalesforceUserDetails(servletRequest, salesforceToken);
 			String email = salesforceUser.getEmail();
