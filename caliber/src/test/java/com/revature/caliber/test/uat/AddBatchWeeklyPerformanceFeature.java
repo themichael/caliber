05396b1fedec07c1ecccdf6bd1eed5551418fc5c
Omokorede <koredeallen@gmail.com>
2017-09-25 10:52:47 -0400

    assess page pom

--- caliber/src/test/java/com/revature/caliber/test/uat/AddBatchWeeklyPerformanceFeature.java
+++ caliber/src/test/java/com/revature/caliber/test/uat/AddBatchWeeklyPerformanceFeature.java
@@ -38,7 +38,7 @@ public class AddBatchWeeklyPerformanceFeature {
 	
 	@Given("^I am in the Assess Batch Page$")
 	public void i_am_in_the_Assess_Batch_Page() throws Throwable {
-		assessBatchPage.goToPage("vp/assess");
+		assessBatchPage.goToPage();
 		assessBatchPage.verifyAssessPage();
 		assessBatchPage.clickCreateAssessment();
 	}
