57cc20c3ef058f2aee023f342af30d91c6e99afd
Patrick <patrick.walsh@revature.com>
2017-09-25 11:55:34 -0400

    tomcat fix

--- caliber/src/main/java/com/revature/caliber/security/impl/AuthorizationImpl.java
+++ caliber/src/main/java/com/revature/caliber/security/impl/AuthorizationImpl.java
@@ -21,11 +21,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.revature.caliber.security.Authorization;
 import com.revature.caliber.security.models.SalesforceUser;
@@ -70,8 +70,8 @@ public class AuthorizationImpl extends AbstractSalesforceSecurityHelper implemen
 	 * @param code
 	 */
 	@RequestMapping("/authenticated")
-	public ModelAndView generateSalesforceToken(@RequestParam(value = "code") String code, Model model)
-			throws IOException {
+	public ModelAndView generateSalesforceToken(@RequestParam(value = "code") String code,
+			RedirectAttributes redirectAttributes) throws IOException {
 		log.debug("in authenticated method");
 		HttpClient httpClient = HttpClientBuilder.create().build();
 		HttpPost post = new HttpPost(loginURL + accessTokenURL);
@@ -84,9 +84,9 @@ public class AuthorizationImpl extends AbstractSalesforceSecurityHelper implemen
 		post.setEntity(new UrlEncodedFormEntity(parameters));
 		log.debug("Generating Salesforce token");
 		HttpResponse response = httpClient.execute(post);
-		model.addAttribute("salestoken", toJsonString(response.getEntity().getContent()));
+		redirectAttributes.addAttribute("salestoken", toJsonString(response.getEntity().getContent()));
 		log.debug("Forwarding to : " + redirectUrl);
-		return new ModelAndView(forwardUrl);
+		return new ModelAndView(REDIRECT + redirectUrl);
 	}
 
 	/**
