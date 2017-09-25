8e620958f019f8382777b603a972e4d3a4e545dd
Omokorede <koredeallen@gmail.com>
2017-09-25 15:17:27 -0400

    assert exam method

--- caliber/src/test/java/com/revature/caliber/test/uat/AddBatchFeature.java
+++ caliber/src/test/java/com/revature/caliber/test/uat/AddBatchFeature.java
@@ -9,87 +9,87 @@ import cucumber.api.java.en.When;
 
 public class AddBatchFeature {
 
-	AssessBatchPage assessBatch;
-	
-	
-	@Before
-	public void setup(){
-		ChromeDriverSetup setup = new ChromeDriverSetup();
-		assessBatch = new AssessBatchPage(setup.getDriver());
-	}
-	
-//	@Given("^I am on the Manage Batch page$")
-//	public void IAmOnTheManageBatchPage() throws Throwable {
-//		// Write code here that turns the phrase above into concrete actions
+//	AssessBatchPage assessBatch;
+//	
+//	
+//	@Before
+//	public void setup(){
+//		ChromeDriverSetup setup = new ChromeDriverSetup();
+//		assessBatch = new AssessBatchPage(setup.getDriver());
+//	}
+//	
+////	@Given("^I am on the Manage Batch page$")
+////	public void IAmOnTheManageBatchPage() throws Throwable {
+////		// Write code here that turns the phrase above into concrete actions
+////
+////	}
 //
+//	@Given("^I have clicked Create Batch$")
+//	public void IHaveClickedCreateBatch() throws Throwable {
+//		// Write code here that turns the phrase above into concrete actions
 //	}
-
-	@Given("^I have clicked Create Batch$")
-	public void IHaveClickedCreateBatch() throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-	}
-
-	@Given("^I have entered \"([^\"]*)\" as the Training name$")
-	public void IHaveEnteredAsTheTrainingName(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-	}
-
-	@Given("^I have selected \"([^\"]*)\" as the Training Type$")
-	public void IHaveSelectedAsTheTrainingType(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-	}
-
-	@Given("^I have selected \"([^\"]*)\" as the Skill type$")
-	public void IHaveSelectedAsTheSkillType(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-	}
-
-	@Given("^I have selected \"([^\"]*)\" as the Location$")
-	public void IHaveSelectedAsTheLocation(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-	}
-
-	@Given("^I have selected \"([^\"]*)\" as the Trainer$")
-	public void IHaveSelectedAsTheTraineri_have_selected_as_the_Trainer(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-	}
-
-	@Given("^I have selected \"([^\"]*)\" as the Co-Trainer$")
-	public void IHaveSelectedAsTheCoTrainer(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-	}
-
-	@Given("^I have selected \"([^\"]*)\" as the Start Date$")
-	public void IHaveSelectedAsTheStartDate(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-	}
-
-	@Given("^I have selected \"([^\"]*)\" as the End Date$")
-	public void IHaveSelectedAsTheEndDate(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-	}
-
-	@Given("^I have entered (\\d+) as the Good Grade$")
-	public void IHaveEnteredAsTheGoodGrade(int arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-	}
-
-	@Given("^I have entered (\\d+) as the Passing Grade$")
-	public void IHaveEnteredAsThePassingGrade(int arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-	}
-
-	@When("^I click the Create Batch Save button$")
-	public void i_click_the_Create_Batch_Save_button() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	}
-
-	@Then("^the New Batch appears$")
-	public void TheNewBatchAppears() throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-	}
-//	@After
-//	public void teardown(){
-//		assessBatch.teardown();
+//
+//	@Given("^I have entered \"([^\"]*)\" as the Training name$")
+//	public void IHaveEnteredAsTheTrainingName(String arg1) throws Throwable {
+//		// Write code here that turns the phrase above into concrete actions
 //	}
+//
+//	@Given("^I have selected \"([^\"]*)\" as the Training Type$")
+//	public void IHaveSelectedAsTheTrainingType(String arg1) throws Throwable {
+//		// Write code here that turns the phrase above into concrete actions
+//	}
+//
+//	@Given("^I have selected \"([^\"]*)\" as the Skill type$")
+//	public void IHaveSelectedAsTheSkillType(String arg1) throws Throwable {
+//		// Write code here that turns the phrase above into concrete actions
+//	}
+//
+//	@Given("^I have selected \"([^\"]*)\" as the Location$")
+//	public void IHaveSelectedAsTheLocation(String arg1) throws Throwable {
+//		// Write code here that turns the phrase above into concrete actions
+//	}
+//
+//	@Given("^I have selected \"([^\"]*)\" as the Trainer$")
+//	public void IHaveSelectedAsTheTraineri_have_selected_as_the_Trainer(String arg1) throws Throwable {
+//		// Write code here that turns the phrase above into concrete actions
+//	}
+//
+//	@Given("^I have selected \"([^\"]*)\" as the Co-Trainer$")
+//	public void IHaveSelectedAsTheCoTrainer(String arg1) throws Throwable {
+//		// Write code here that turns the phrase above into concrete actions
+//	}
+//
+//	@Given("^I have selected \"([^\"]*)\" as the Start Date$")
+//	public void IHaveSelectedAsTheStartDate(String arg1) throws Throwable {
+//		// Write code here that turns the phrase above into concrete actions
+//	}
+//
+//	@Given("^I have selected \"([^\"]*)\" as the End Date$")
+//	public void IHaveSelectedAsTheEndDate(String arg1) throws Throwable {
+//		// Write code here that turns the phrase above into concrete actions
+//	}
+//
+//	@Given("^I have entered (\\d+) as the Good Grade$")
+//	public void IHaveEnteredAsTheGoodGrade(int arg1) throws Throwable {
+//		// Write code here that turns the phrase above into concrete actions
+//	}
+//
+//	@Given("^I have entered (\\d+) as the Passing Grade$")
+//	public void IHaveEnteredAsThePassingGrade(int arg1) throws Throwable {
+//		// Write code here that turns the phrase above into concrete actions
+//	}
+//
+//	@When("^I click the Create Batch Save button$")
+//	public void i_click_the_Create_Batch_Save_button() throws Throwable {
+//	    // Write code here that turns the phrase above into concrete actions
+//	}
+//
+//	@Then("^the New Batch appears$")
+//	public void TheNewBatchAppears() throws Throwable {
+//		// Write code here that turns the phrase above into concrete actions
+//	}
+////	@After
+////	public void teardown(){
+////		assessBatch.teardown();
+////	}
 }
