022c42ffcd8b4ffaca49ec1f2563805f56ce7623
Omokorede <koredeallen@gmail.com>
2017-09-27 10:07:35 -0400

    naming conventions

--- caliber/src/test/java/com/revature/caliber/test/uat/MakeUserAQualityAuditFeature.java
+++ caliber/src/test/java/com/revature/caliber/test/uat/MakeUserAQualityAuditFeature.java
@@ -6,7 +6,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class MakeUserAQualityAudit {
+public class MakeUserAQualityAuditFeature {
 
 	private SettingsTrainerPage trainerPage;
 	private ChromeDriverSetup setup;
