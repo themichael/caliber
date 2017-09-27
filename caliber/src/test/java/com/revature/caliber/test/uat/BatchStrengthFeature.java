c5fa196479a23f7aeaaf1f4ef737d7dfbdccf243
Patrick <patrick.walsh@revature.com>
2017-09-27 18:55:32 -0400

    code smells

--- caliber/src/test/java/com/revature/caliber/test/uat/BatchStrengthFeature.java
+++ caliber/src/test/java/com/revature/caliber/test/uat/BatchStrengthFeature.java
@@ -14,34 +14,31 @@ public class BatchStrengthFeature {
 		ChromeDriverSetup setup = new ChromeDriverSetup();
 		reportsPage = new ReportsPage(setup.getDriver());
 	}
-
 	
 	@Given("^that I am logged in as a user$")
-	public void that_I_am_logged_in_as_a_user() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
+	public void thatIAmLoggedInAsAUser()  {
 		reportsPage.gotoReportsPage();
  	}
 
 	@Given("^inside the Reports page$")
-	public void inside_the_Reports_page() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
+	public void insideTheReportsPage() {
 		reportsPage.verifyReportsPage();
  	}
 
 	@Given("^I have selected the year$")
-	public void i_have_selected_the_year() throws Throwable {
+	public void iHaveSelectedTheYear() throws InterruptedException {
 		Thread.sleep(500);
 		reportsPage.clickReportYear("year");
  	}
 
 	@When("^I have selected the Batch$")
-	public void i_have_selected_the_Batch() throws Throwable {
+	public void iHaveSelectedTheBatch() {
 		reportsPage.clickBatchDropdown();
 		reportsPage.chooseBatch("Patrick Walsh - 2/14/17");
  	}
 
 	@Then("^I am able to report the Batch performance$")
-	public void i_am_able_to_report_the_Batch_performance() throws Throwable {
+	public void iAmAbleToReportTheBatchPerformance()  {
 		reportsPage.checkTechSkillsGraph();
  	}
 }
