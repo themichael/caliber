53f38f83009ce0334e4007bc64e7d6ea0a41ab34
Connor Monson <connor.monson@yahoo.com>
2017-10-05 20:22:18 -0400

    Added PanelDAO

---
+++ caliber/src/main/java/com/revature/caliber/data/PanelDAO.java
@@ -0,0 +1,105 @@
+package com.revature.caliber.data;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.hibernate.Criteria;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.revature.caliber.beans.Panel;
+import com.revature.caliber.beans.PanelRole;
+
+@Repository
+public class PanelDAO {
+
+	private static final Logger log = Logger.getLogger(PanelDAO.class);
+	private SessionFactory sessionFactory;
+
+	@Autowired
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	/**
+	 * Find all panels titles to be displayed on front end
+	 * 
+	 * 
+	 * 
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	@Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+	public List<String> findAllPanelTitles() {
+		String hql = "select distinct title FROM Panel";
+		return sessionFactory.getCurrentSession().createQuery(hql).list();
+	}
+
+	/**
+	 * Find all panels. Useful for listing available panels
+	 * 
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	@Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+	public List<Panel> findAll() {
+		log.info("Finding all panels");
+		return sessionFactory.getCurrentSession().createCriteria(Panel.class)
+				.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY).list();
+	}
+
+	/**
+	 * 
+	 * Convenience method only. Not practical in production since panels must
+	 * be registered in the Salesforce with a matching email address.
+	 * 
+	 * @param panel
+	 */
+	@Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+	public void save(Panel panel) {
+		log.info("Save panel " + panel);
+		sessionFactory.getCurrentSession().save(panel);
+	}
+
+	/**
+	 * Find panel by the given identifier
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+	public Panel findOne(Integer panelId) {
+		log.info("Find panel by id: " + panelId);
+		return (Panel) sessionFactory.getCurrentSession().createCriteria(Panel.class)
+				.add(Restrictions.eq("panelId", panelId)).uniqueResult();
+	}
+
+	/**
+	 * Updates a panel in the database.
+	 * 
+	 * @param panel
+	 */
+	@Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+	public void update(Panel panel) {
+		log.info("Update panel " + panel);
+		sessionFactory.getCurrentSession().saveOrUpdate(panel);
+	}
+
+	/**
+	 * Convenience method only. Deletes a panel from the database. Panel
+	 * will still be registered with a Salesforce account.
+	 * 
+	 * @param panel
+	 */
+	@Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+	public void delete(Panel panel) {
+		log.info("Delete panel " + panel);
+		sessionFactory.getCurrentSession().delete(panel);
+	}
+
+}
