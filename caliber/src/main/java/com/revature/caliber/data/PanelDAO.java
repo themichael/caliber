9dab962cdb2fdbdea5ba6d178fe0f26e50e40dde
Connor Monson <connor.monson@yahoo.com>
2017-10-06 08:57:54 -0400

    Minor fix

--- caliber/src/main/java/com/revature/caliber/data/PanelDAO.java
+++ caliber/src/main/java/com/revature/caliber/data/PanelDAO.java
@@ -13,7 +13,6 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.revature.caliber.beans.Panel;
-import com.revature.caliber.beans.PanelRole;
 
 @Repository
 public class PanelDAO {
