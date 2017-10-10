9fcd700d8ff61be98ae488b81add86d2cc16c409
koszuta <koszutan94@gmail.com>
2017-10-10 11:14:42 -0400

    added imports to PanelAPITest

--- caliber/src/test/java/com/revature/caliber/test/api/PanelAPITest.java
+++ caliber/src/test/java/com/revature/caliber/test/api/PanelAPITest.java
@@ -3,6 +3,9 @@ package com.revature.caliber.test.api;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.revature.caliber.data.PanelDAO;
+import com.revature.caliber.data.PanelFeedbackDAO;
+
 public class PanelAPITest extends AbstractAPITest {
 	
 	private static final Logger log = Logger.getLogger(PanelAPITest.class);
