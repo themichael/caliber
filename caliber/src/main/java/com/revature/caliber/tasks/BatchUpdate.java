95331972a7dc3dcbf4583bde271fbd6aa331ed5d
agarnelo <garnelo.anahi@gmail.com>
2017-11-17 17:07:11 -0500

    Spring Task under context

--- caliber/src/main/java/com/revature/caliber/tasks/BatchUpdate.java
+++ caliber/src/main/java/com/revature/caliber/tasks/BatchUpdate.java
@@ -1,12 +1,16 @@
 package com.revature.caliber.tasks;
 
+import java.util.Date;
+import java.util.logging.*;
+
 import org.springframework.scheduling.annotation.Scheduled;
 
 public class BatchUpdate {
-
+	private int i = 0;
 	//@Scheduled(cron = "0 0 0 * * *")  //Midnight
 	@Scheduled(cron = "0 * * * * *") 	//Every minute (testing)
 	public void updateBatchTask() {
-		//Update batch job 
+		//Update job goes here
 	}
+
 }
