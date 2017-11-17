7cee0da7b4f75667424f0e20e261b2902bc2ba3b
agarnelo <garnelo.anahi@gmail.com>
2017-11-17 15:10:55 -0500

    Integrated Spring Task with @Scheduled annotation

---
+++ caliber/src/main/java/com/revature/caliber/tasks/BatchUpdate.java
@@ -0,0 +1,12 @@
+package com.revature.caliber.tasks;
+
+import org.springframework.scheduling.annotation.Scheduled;
+
+public class BatchUpdate {
+
+	//@Scheduled(cron = "0 0 0 * * *")  //Midnight
+	@Scheduled(cron = "0 * * * * *") 	//Every minute (testing)
+	public void updateBatchTask() {
+		//Update batch job 
+	}
+}
