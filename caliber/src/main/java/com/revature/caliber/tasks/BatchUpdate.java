17cef6ed440b8ac4eb53f7c58e5942b712cc54c1
agarnelo <garnelo.anahi@gmail.com>
2017-11-17 22:11:14 -0500

    Removed unused imports

--- caliber/src/main/java/com/revature/caliber/tasks/BatchUpdate.java
+++ caliber/src/main/java/com/revature/caliber/tasks/BatchUpdate.java
@@ -1,16 +1,11 @@
 package com.revature.caliber.tasks;
 
-import java.util.Date;
-import java.util.logging.*;
-
 import org.springframework.scheduling.annotation.Scheduled;
 
 public class BatchUpdate {
-	private int i = 0;
 	//@Scheduled(cron = "0 0 0 * * *")  //Midnight
 	@Scheduled(cron = "0 * * * * *") 	//Every minute (testing)
 	public void updateBatchTask() {
 		//Update job goes here
 	}
-
 }
