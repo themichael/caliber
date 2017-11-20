206dde1c8011f8e80708ac78a3cc92115082ea26
victoriachenn <victoriachennn@gmail.com>
2017-11-19 19:44:52 -0500

    BatchUpdate with testing

--- caliber/src/main/java/com/revature/caliber/tasks/BatchUpdate.java
+++ caliber/src/main/java/com/revature/caliber/tasks/BatchUpdate.java
@@ -3,6 +3,12 @@ package com.revature.caliber.tasks;
 import org.springframework.scheduling.annotation.Scheduled;
 
 public class BatchUpdate {
+	
+	/*
+	 * Test Method: Used cron to perform midnight execution 
+	 * 				To update batches
+	 */
+	
 	//@Scheduled(cron = "0 0 0 * * *")  //Midnight
 	@Scheduled(cron = "0 * * * * *") 	//Every minute (testing)
 	public void updateBatchTask() {
