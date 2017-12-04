be49d26ba5d289f486cefb1cb32e5f5240770851
carolynrehm <32271621+carolynrehm@users.noreply.github.com>
2017-12-03 19:20:33 -0500

    remove log

--- caliber/src/main/java/com/revature/caliber/email/Mailer.java
+++ caliber/src/main/java/com/revature/caliber/email/Mailer.java
@@ -175,7 +175,6 @@ public class Mailer implements Runnable {
 	public Set<Trainer> getTrainersWhoNeedToSubmitGrades() {
 		Set<Trainer> trainersToSubmitGrades = new HashSet<Trainer>();
 		List<Batch> batches = getBatches();
-		logger.info(batches + " pineapple ");
 		for (Batch batch : batches) {
 			Set<Trainee> trainees = batch.getTrainees();
 			//The following removes all dropped trainees from the Trainee Set
