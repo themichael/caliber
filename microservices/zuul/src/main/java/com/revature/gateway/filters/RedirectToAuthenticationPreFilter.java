92bb61821bdba2a59592d908a7380c7e8a242595
Cooper Cummings <coopercummings6@gmail.com>
2018-01-30 11:27:11 -0500

    modified filter to allow requests that already have an authenticated cookie to bypass authentication

--- microservices/zuul/src/main/java/com/revature/gateway/filters/RedirectToAuthenticationPreFilter.java
+++ microservices/zuul/src/main/java/com/revature/gateway/filters/RedirectToAuthenticationPreFilter.java
@@ -22,45 +22,59 @@ public class RedirectToAuthenticationPreFilter extends ZuulFilter {
     @Override
     public Object run() {
         RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
         String contextURI = (String) context.get("requestURI");
-        
-        context.put("preRedirectRequestUri", contextURI);
 
-        //these endpoints should bypass authentication
-        if(contextURI.contains("/security/test"))
+        Cookie[] cookies = request.getCookies();
+        boolean hasRole = false;
+        for(cookie c : cookies)
         {
-            context.put("requestURI", "/security/authorize");
+            if(c.getName().equals("role"))
+            {
+                hasRole = true;
+            }
         }
-        else if(contextURI.contains("/revoke"))
+        
+        if(hasRole)//if the user has already logged in, they will have a cookie named role
         {
-            context.put("requestURI", "forward:/security/revoke");
-        } 
-        else if(contextURI.contains("/revoke"))
-        {
-            context.put("requestURI", "forward:/security/revoke");
+            context.put("preRedirectRequestUri", contextURI);
+
+            //these endpoints should bypass authentication
+            if(contextURI.contains("/security/test"))
+            {
+                context.put("requestURI", "/security/authorize");
+            }
+            else if(contextURI.contains("/revoke"))
+            {
+                context.put("requestURI", "forward:/security/revoke");
+            } 
+            else if(contextURI.contains("/revoke"))
+            {
+                context.put("requestURI", "forward:/security/revoke");
+            }
+            else if(contextURI.contains("/authenticated_token"))
+            {
+                context.put("requestURI", "forward:/security/authenticated_token");
+            }
+            else if(contextURI.contains("/authenticated"))
+            {
+                context.put("requestURI", "forward:/security/authenticated");
+            }
+            else if
+            // add code to exclude localhost:8080/ nothing or caliber/ nothing eventually
+            (System.getEnv("CALIBER_DEV_MODE").equals("true"))
+            {
+                if(contextURI.length() > 14 && contextURI.substring(contextURI.length() - 15).equals("localhost:8080/"))//ends with localhost:8080/
+                context.put("requestURI", "forward:/security/");
+            }
+            else
+            {
+                if(contextURI.length() > 7 && contextURI.substring(contextURI.length() - 8).equals("caliber/"))//ends with caliber/
+                context.put("requestURI", "forward:/security/");
+            }
+            else
+                context.put("requestURI", "forward:/security/authorize");//anything that is not one of those endpoints goes here
         }
-        else if(contextURI.contains("/authenticated_token"))
-        {
-            context.put("requestURI", "forward:/security/authenticated_token");
-        }
-        else if(contextURI.contains("/authenticated"))
-        {
-            context.put("requestURI", "forward:/security/authenticated");
-        }
-        else if
-        // add code to exclude localhost:8080/ nothing or caliber/ nothing eventually
-        (System.getEnv("CALIBER_DEV_MODE").equals("true"))
-        {
-            if(contextURI.length() > 14 && contextURI.substring(contextURI.length() - 15).equals("localhost:8080/"))//ends with localhost:8080/
-            context.put("requestURI", "forward:/security/");
-        }
-        else
-        {
-            if(contextURI.length() > 7 && contextURI.substring(contextURI.length() - 8).equals("caliber/"))//ends with caliber/
-            context.put("requestURI", "forward:/security/");
-        }
-        else
-            context.put("requestURI", "forward:/security/authorize");//anything that is not one of those endpoints goes here
         return null;
     }
  
