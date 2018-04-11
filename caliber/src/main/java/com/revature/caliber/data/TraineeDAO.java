a8df4a1f2ac95867ed725bde249a3c07fd086273
Patrick Walsh <patrick.walsh@revature.com>
2018-04-11 17:32:44 -0400

    I am now a human to August

--- caliber/src/main/java/com/revature/caliber/data/TraineeDAO.java
+++ caliber/src/main/java/com/revature/caliber/data/TraineeDAO.java
@@ -191,7 +191,6 @@ public class TraineeDAO {
 	 */
 	@Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
 	public void update(Trainee trainee) {
-		log.info("Update trainee: " + trainee);
 		sessionFactory.getCurrentSession().saveOrUpdate(trainee);
 	}
 
