9ed40089b1703e66919f73964ef1b5a4b1b8773b
Patrick Walsh <patrick.walsh@revature.com>
2018-05-09 11:43:50 -0400

    skill type conversion

--- caliber/src/main/java/com/revature/caliber/salesforce/SalesforceTransformerToCaliber.java
+++ caliber/src/main/java/com/revature/caliber/salesforce/SalesforceTransformerToCaliber.java
@@ -96,6 +96,15 @@ public class SalesforceTransformerToCaliber {
 	}
 
 	private SkillType transformSkillTypeHelper(String skillType) {
+		if(skillType.contains("Java")) {
+			return SkillType.J2EE;
+		}
+		if(skillType.contains(".NET")) {
+			return SkillType.NET;
+		}
+		if(skillType.contains("JTA")) {
+			return SkillType.JTA;
+		}
 		switch (skillType) {
 		case "Full Stack Java/JEE":
 			return SkillType.J2EE;
