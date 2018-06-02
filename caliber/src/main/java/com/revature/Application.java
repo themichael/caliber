1b2f5f7dc6bb9c3645f6ea7555b4f97f74ac3088
Emily Higgins <emilyrchiggins@gmail.com>
2018-06-02 18:07:10 -0400

    fun with dependencies

--- caliber/src/main/java/com/revature/Application.java
+++ caliber/src/main/java/com/revature/Application.java
@@ -1,13 +1,24 @@
 package com.revature;
 
+import javax.persistence.EntityManagerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
 
 @SpringBootApplication
 public class Application {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	public HibernateJpaSessionFactoryBean sessionFactory(EntityManagerFactory emf) {
+	    HibernateJpaSessionFactoryBean factory = new HibernateJpaSessionFactoryBean();
+	    factory.setEntityManagerFactory(emf);
+	    return factory;
 	}
 
 }
