450ac5df95e928034a95ce60226988be5e970570
Emily Higgins <emilyrchiggins@gmail.com>
2018-06-04 01:32:03 -0400

    beginning of BatchRepository

--- caliber/src/main/java/com/revature/caliber/data/BatchRepository.java
+++ caliber/src/main/java/com/revature/caliber/data/BatchRepository.java
@@ -1,8 +1,11 @@
 package com.revature.caliber.data;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.revature.caliber.beans.Assessment;
 import com.revature.caliber.beans.Batch;
 
 /**
@@ -12,5 +15,57 @@ import com.revature.caliber.beans.Batch;
  */
 @Repository
 public interface BatchRepository extends JpaRepository<Batch, Integer>{
+	
+	/**
+	 * Save an batch to the database
+	 *
+	 * @param batch
+	 * @return updated batch
+	 */
+	@SuppressWarnings("unchecked")
+	public Batch save(Batch batch);
+
+	/**
+	 * Find batch with the specified id, including trainees who have been dropped
+	 * 
+	 * @param id
+	 * @return batch
+	 */
+	public Batch findOne(int id);
+	
+	/**
+	 * Find batch with the specified id, NOT including trainees who have been dropped
+	 * TODO
+	 * @param id
+	 * @return batch
+	 */
+	
+	/**
+	 * Find all current batches, NOT including trainees who have been dropped
+	 * TODO
+	 * @return batch
+	 */
+	
+	/**
+	 * Find all current batches for the specified trainer or co-trainer, 
+	 * NOT including trainees who have been dropped
+	 * TODO
+	 * @param trainerId
+	 * @return list of batches
+	 */
+	
+	
+	
+	/**
+	 * Remove the specified batch from the database
+	 * 
+	 * @param batch
+	 */
+	public void delete(Batch batch);
+
+	
+	
+
+	
 
 }
