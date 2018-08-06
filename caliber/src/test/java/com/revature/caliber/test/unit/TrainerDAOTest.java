06d860a85fddac9443dce68723fb61e9ad9ec1b4
Patrick Walsh <patrick.walsh@revature.com>
2018-08-05 22:08:42 -0400

    revpro sync

--- caliber/src/test/java/com/revature/caliber/test/unit/TrainerDAOTest.java
+++ caliber/src/test/java/com/revature/caliber/test/unit/TrainerDAOTest.java
@@ -45,7 +45,7 @@ public class TrainerDAOTest extends CaliberTest {
 	@Test
 	public void testFindAll() {
 		log.debug("Find all trainers");
-		assertEquals(27, trainerDAO.findAll().size());
+		assertEquals(30, trainerDAO.findAll().size());
 	}
 
 	@Test
